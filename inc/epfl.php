5a15238ed4b91e3368879c0350be5da433d217e5
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-10-27 10:58:35 +0200

    Whoops, we don't want a POST here.

--- inc/epfl.php
+++ inc/epfl.php
@@ -6,7 +6,6 @@
 function curl_get( $url ) {
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
