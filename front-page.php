9372019ed6b272c34e974ce05c8567ff93369c26
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-10-27 10:43:26 +0200

    A starter front page.
    I got the idea from here:
    https://developer.wordpress.org/themes/basics/template-hierarchy/

---
+++ front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package understrap
+ */
+
+get_header();
+
+$container   = get_theme_mod( 'understrap_container_type' );
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+
+## You can look at the output of error_log with the following command:
+# docker exec -it jahia2wp_httpd_1 tail  -fsrv/test/logs/error_log
+error_log("is_front_page() is " + is_front_page());
+error_log("is_home() is " + is_home());
+
+?>
+
+<p>This is the home page you are looking for.</p>
+
+<?php get_footer(); ?>
+
