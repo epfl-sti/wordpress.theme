354ea4b870b17ec18ce5c7fd409f62520c22e736
Dominique Quatravaux <dominique@quatravaux.org>
2017-10-30 18:57:15 +0100

    Ability to transclude things in a style-able way
    * New function html_body() to extract only the body of an HTML
      document (as seen on https://stackoverflow.com/a/11254965/435004)
    * Application example in front-page.php
    * Minor tweak to header comment in same

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the front page.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -15,22 +15,17 @@ get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
-## You can look at the output of error_log with the following command:
-# docker exec -it jahia2wp_httpd_1 tail  -fsrv/test/logs/error_log
-error_log("is_front_page() is " + is_front_page());
-error_log("is_home() is " + is_home());
-
 ?>
 <br><br><br>
 <img width=100% src="<?php echo get_stylesheet_directory_uri(); ?>/img/LacourTeam.jpg">
 
-<div 
-
-
-
-<?php //echo curl_get("https://stisrv13.epfl.ch/cgi-bin/whoop/thunderbird.pl?id=researchvideo&lang=eng&thunderbird=researchvideo");
+<div class="from-stisrv13">
+<?php echo html_body(curl_get("https://stisrv13.epfl.ch/cgi-bin/whoop/thunderbird.pl?id=researchvideo&lang=eng&thunderbird=researchvideo"));
 
 ?>
+</div>
+
+<!-- end transclusion -->
 
 <?php get_footer(); ?>
 
