fdc2ea564d837bfba59e1e7ec2a32e4db0575535
Loïc Humbert <loic.humbert@epfl.ch>
2017-10-31 09:45:45 +0100

    Display 20 news with flexboxes

--- front-page.php
+++ front-page.php
@@ -23,15 +23,25 @@ error_log("is_home() is " + is_home());
 ?>
 <img width=100% src="<?php echo get_stylesheet_directory_uri(); ?>/img/LacourTeam.jpg" style="padding-top: 40px">
 
+<style>
+  div.news {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+</style>
+
 <div class="news">
-  <div class="card" style="width: 20rem;">
-  <img class="card-img-top" src="https://placehold.it/500x400" alt="Card image cap">
-  <div class="card-body">
-    <h4 class="card-title">Card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  </div>
+  <?php for ($i=0; $i < 20; $i++) { ?>
+    <div class="card" style="width: 20rem;">
+    <img class="card-img-top" src="https://placehold.it/500x400" alt="Card image cap">
+    <div class="card-body">
+      <h4 class="card-title">EPFL</h4>
+      <p class="card-text">This is the new #<?php echo $i?></p>
+      <a href="#" class="btn btn-primary">Read more</a>
+    </div>
+    </div>
+  <?php } ?>
 </div>
 
 
