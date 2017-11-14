6718cb83aef73b0a0d6d5a18904a564750dab5a9
ponsfrilus <ponsfrilus@gmail.com>
2017-11-14 22:21:13 +0100

    Formatting improvements

--- header.php
+++ header.php
@@ -12,14 +12,14 @@ $container = get_theme_mod( 'epflsti_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-        <link rel="profile" href="http://gmpg.org/xfn/11">
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
     <!-- Custom -->
     <!-- CSS -->
@@ -32,7 +32,7 @@ $container = get_theme_mod( 'epflsti_container_type' );
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/modernizr.js"></script>
     <!-- end custom -->
 
-        <?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -40,42 +40,50 @@ $container = get_theme_mod( 'epflsti_container_type' );
 <!-- Ugly hard coded menu -->
 <div class="menu-container">
     <div class="menu">
-
-            <!-- Your site logo -->
-            <a href="https://www.epfl.ch">
-            <?php if ( ! has_custom_logo() ) { ?>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/epfl.svg" width=180 style="float:left; margin: 10px" />
-            <?php } else {
-                    the_custom_logo();
-            } ?><!-- end custom logo -->
-            </a>
-	   <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><acronym title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" style="text-decoration: none;"><?php bloginfo( 'name' ); ?></acronym></a> 
+        <!-- Your site logo -->
+        <a href="https://www.epfl.ch">
+        <?php
+        if (! has_custom_logo()) {  ?>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/epfl.svg" width=180 style="float:left; margin: 10px" />
+        <?php
+        } else {
+            the_custom_logo();
+        } ?><!-- end custom logo -->
+        </a>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <acronym title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" style="text-decoration: none;">
+                <?php bloginfo( 'name' ); ?>
+            </acronym>
+        </a>
         <ul>
-            <li><a class="mylink" href="#">About</a>
+            <li>
+                <a href="#" class="sti_menu_link">About</a>
                 <ul>
-                    <li><a href="#">News</a>
+                    <li>
+                        <a href="#">News</a>
                         <ul>
                             <li><a href="#">STI in the News</a></li>
                             <li><a href="#">News Archives</a></li>
-                            <li></li>
                         </ul>
                     </li>
-                    <li><a href="#">Seminars</a>
+                    <li>
+                        <a href="#">Seminars</a>
                         <ul>
                             <li><a href="#">Events for Future Students</a></li>
                         </ul>
                     </li>
-                    <li><a href="#">Centres</a>
+                    <li>
+                        <a href="#">Centres</a>
                         <ul>
                             <li><a href="#">National Research Centres</a></li>
                             <li><a href="#">Research Centres</a></li>
                             <li><a href="#">Platforms and Services</a></li>
                             <li><a href="#">Centres in the News</a></li>
                             <li><a href="#">Centres Events</a></li>
-                            <li></li>
                         </ul>
                     </li>
-                    <li><a href="#">Campuses</a>
+                    <li>
+                        <a href="#">Campuses</a>
                         <ul>
                             <li><a href="#">Main campus</a></li>
                             <li><a href="#">Sion</a></li>
@@ -85,8 +93,13 @@ $container = get_theme_mod( 'epflsti_container_type' );
                             <li><a href="#">Lausanne</a></li>
                         </ul>
                     </li>
-                    <li><div style="float:left"><img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/sti<?php echo rand(1,4); ?>.jpg" /></div></li>
-                    <li><a href="#">Administration</a>
+                    <li>
+                        <div style="float:left">
+                            <img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/sti<?php echo rand(1,4); ?>.jpg" />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">Administration</a>
                         <ul>
                             <li><a href="#">Faculty Direction</a></li>
                             <li><a href="#">Organisational Chart</a></li>
@@ -95,40 +108,63 @@ $container = get_theme_mod( 'epflsti_container_type' );
                             <li></li>
                         </ul>
                     </li>
-                    <li><a href="#">Contact</a>
+                    <li>
+                        <a href="#">Contact</a>
                     </li>
                 </ul>
             </li>
-            <li><a href="#" class=mylink>Teaching</a>
+            <li>
+                <a href="#" class="sti_menu_link">Teaching</a>
                 <ul>
-                    <li> <ul> <li><div style="float:left"><img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/education<?php echo rand(1,2); ?>.jpg" /></div></li> </ul> </li>
-                    <li><a href="#">Bachelor</a>
+                    <li>
+                        <ul>
+                            <li>
+                                <div style="float:left">
+                                    <img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/education<?php echo rand(1,2); ?>.jpg" />
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Bachelor</a>
                         <ul>
                     	    <li><a href="#">Semester Projects</a></li>
-			</ul>
+                        </ul>
                     </li>
-                    <li><a href="#">Master</a>
-			<ul>
+                    <li>
+                        <a href="#">Master</a>
+                        <ul>
                     	    <li><a href="#">Master Projects</a></li>
                     	    <li><a href="#">Placements in Industry</a></li>
-			</ul>
-		    </li>
-                    <li><a href="#">PhD</a>
-			<ul>
-                    	    <li><a href="#">Doctoral Programs</a></li>
-		            <li></li>
-		            <li></li>
-			</ul>
+                    	</ul>
                     </li>
-                    <li><a href="#">Testimonials</a></li>
+                    <li>
+                        <a href="#">PhD</a>
+                        <ul>
+                    	    <li><a href="#">Doctoral Programs</a></li>
+                            <li>...</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Testimonials</a>
+                    </li>
                 </ul>
             </li>
-            <li><a href="#" class=mylink>Research</a>
-
+            <li>
+                <a href="#" class="sti_menu_link">Research</a>
                 <ul>
-                    <li> <ul> <li><div style="float:left"><img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/research<?php echo rand(1,2); ?>.jpg" /></div></li> </ul> </li>
-
-                    <li><a href="">Faculty</a>
+                    <li>
+                        <ul>
+                            <li>
+                                <div style="float:left">
+                                    <img width=220 src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/research<?php echo rand(1,2); ?>.jpg" />
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Faculty</a>
                         <ul>
                             <li><a href="#">Open Faculty Positions</a></li>
                             <li><a href="#">Faculty Announcements</a></li>
@@ -138,19 +174,20 @@ $container = get_theme_mod( 'epflsti_container_type' );
                             <li><a href="#">Faculty Affairs</a></li>
                         </ul>
                     </li>
-
-                    <li><a href="#">Our Institutes</a>
-                     <ul>
-                      <li><a href="#">Bioengineering</a></li>
-                      <li><a href="#">Electrical Engineering</a></li>
-                      <li><a href="#">Materials Science and Engineering</a></li>
-                      <li><a href="#">Mechanical Engineering</a></li>
-                      <li><a href="#">Microengineering</a></li>
-                     </ul>
+                    <li>
+                        <a href="#">Our Institutes</a>
+                        <ul>
+                            <li><a href="#">Bioengineering</a></li>
+                            <li><a href="#">Electrical Engineering</a></li>
+                            <li><a href="#">Materials Science and Engineering</a></li>
+                            <li><a href="#">Mechanical Engineering</a></li>
+                            <li><a href="#">Microengineering</a></li>
+                        </ul>
                     </li>
                 </ul>
             </li>
-            <li><a href="#" class=mylink>Innovation</a>
+            <li>
+                <a href="#" class="sti_menu_link">Innovation</a>
                 <ul>
                     <li>
                         <ul>
@@ -164,31 +201,39 @@ $container = get_theme_mod( 'epflsti_container_type' );
                     <li><a href="#">Patents</a></li>
                     <li><a href="#">Master Projects in Industry</a></li>
                     <li><a href="#">Features</a></li>
-                    <li> <ul> <li><div style="float:left"></div></li> </ul> </li>
-			
-                    <li><a href="#">Technology Transfer</a>
-			<ul>
-                    	    <li><a href="#">Features</a></li>
-			    <li><a href="#">Patents</a></li>
-			</ul>
-		    </li>
-                    <li><a href="#">Master Projects in Industry</a>
-			<ul>
- 				<li><a href="#">Definition</a></li>
- 				<li><a href="#">Participating sections</a></li>
- 				<li><a href="#">Financial Support to Industry</a></li>
- 				<li><a href="#">Submit a project</a></li>
- 				<li><a href="#">Master Placements Archive</a></li>
-			</ul>
-		    </li>
-		    <li><a href="#">Industry Day</a>
-			<ul>
-			<li><a href="#">2017 Event Pictures</a></li>
-				<li><a href="#">Filmed Talks 2017</a></li>
-				<li><a href="#">2017 Program</a></li>
-				<li><a href="#">2016 archive</a></li>
-			</ul>
-		    </li>
+                    <li>
+                        <ul>
+                            <li>
+                                <div style="float:left"></div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Technology Transfer</a>
+                        <ul>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Patents</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Master Projects in Industry</a>
+                        <ul>
+                            <li><a href="#">Definition</a></li>
+                            <li><a href="#">Participating sections</a></li>
+                            <li><a href="#">Financial Support to Industry</a></li>
+                            <li><a href="#">Submit a project</a></li>
+                            <li><a href="#">Master Placements Archive</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Industry Day</a>
+                        <ul>
+                            <li><a href="#">2017 Event Pictures</a></li>
+                            <li><a href="#">Filmed Talks 2017</a></li>
+                            <li><a href="#">2017 Program</a></li>
+                            <li><a href="#">2016 archive</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </li>
         </ul>
