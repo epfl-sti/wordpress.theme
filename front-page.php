c225cbc9810a32e83ba50b2f2b8ed277e28b0d8e
ponsfrilus <ponsfrilus@gmail.com>
2017-11-21 14:38:32 +0100

    Working "in line style" events in slider

--- front-page.php
+++ front-page.php
@@ -19,27 +19,59 @@ get_header();
         <div id="containercalendar">
             <table cellpadding="16" style="background-image:url('https://stisrv13.epfl.ch/proposals/darkpixel.png');">
                 <td>
-                    <table>
+                    <table style="border-collapse: separate;border-spacing: 5px 8px;">
                         <tr>
                             <td>
                                 <a href="#">
                                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/upcoming_events.png" />
-                                    <br />
-                                    <br />
                                 </a>
                             </td>
                         </tr>
-                        <tr>
-                            <td width="280" style="background-color:#fff; padding: 8px;">
-                                Events
-                            </td>
-                        </tr>
+                        <?php
+                          $events = get_events($url='https://memento.epfl.ch/api/jahia/mementos/sti/events/en/?format=json', $limit=4);
+                          foreach ($events as $event) {
+                             $event_day = date("d", strtotime($event->event_start_date));
+                             $event_month = strtoupper(date("M", strtotime($event->event_start_date)));
+                        ?>
+                            <tr class="slider-event-row" data-link="<?php echo $event->absolute_slug; ?>">
+                                <td class="slider-event-cell" width="280" style="background-color:#fff; padding: 8px">
+                                    <div class="slider-event-date" style="width:32px;display:block;float:left;border: 1px solid #55576A; padding: 4px">
+                                       <span class="slider-event-date-day" style="display:block;text-align:center;font-size:0.6em">
+                                          <?php echo $event_day; ?>
+                                       </span>
+                                       <span class="slider-event-date-month" style="display:block;text-align:center;font-size:0.5em">
+                                          <?php echo $event_month; ?>
+                                       </span>
+                                    </div>
+                                    <div class="slider-event-title" style="display:block; float:right;font-size:0.9em">
+                                       <?php echo $event->title; ?><br />
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php
+                          }
+                         ?>
                     </table>
                 </td>
             </table>
         </div>
     </div>
 </div>
+<script>
+  $( "div.slider-event-date" )
+    .mouseenter(function() {
+      $( this ).css( { backgroundColor: "#55576A", color: "#FA2400", "font-weight": "bold" })
+    })
+    .mouseleave(function() {
+      $( this ).css( { backgroundColor: "#fff", color: "#000", "font-weight": "normal" })
+    });
+  $( "tr.slider-event-row" )
+    .click(function() {
+      window.location = $( this ).data("link");
+      return true;
+     });
+</script>
+
 <div class="div-wrapper" id="containerwave" style="">
    <div class="pixelman">
       <div id="sti-homecarousel" class="carousel slide" data-ride="carousel">
--- inc/epfl.php
+++ inc/epfl.php
@@ -3,26 +3,36 @@
  * EPFL-specific functions
  */
 
-function curl_get( $url ) {
+function curl_get($url)
+{
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $output = curl_exec($ch);       
+  $output = curl_exec($ch);
   curl_close($ch);
   return $output;
 }
 
-function html_body( $html ) {
-    $d = new DOMDocument;
-    $d->loadHTML($html);
-    $body = $d->getElementsByTagName('body')->item(0);
-    $ret = "";
-    foreach ($body->childNodes as $childNode) {
-        $ret .= $d->saveHTML($childNode);
-    }
-    return $ret;
+function html_body($html)
+{
+  $d = new DOMDocument;
+  $d->loadHTML($html);
+  $body = $d->getElementsByTagName('body')->item(0);
+  $ret = "";
+  foreach ($body->childNodes as $childNode) {
+    $ret .= $d->saveHTML($childNode);
+  }
+  return $ret;
 }
 
-?>
+/* Retrieve events from the Memento REST api */
+// Note: idk what's the "published" entry, but it would be nicer to be sure
+//       to only fetch those one.
+function get_events($url='https://memento.epfl.ch/api/jahia/mementos/sti/events/en/?format=json', $limit=4)
+{
+  $data = curl_get($url . '&limit=' . $limit);
+  $data = json_decode($data);
+  return $data;
+}
