efc6a4dc37f255744092778eb6767c72280537d5
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-01 15:58:21 +0100

    Starter kit for a Vue-in-WP-admin app
    In package.json:
    * Add vue shiny toys
    
    In gulpfile.js (and .babelrc):
    * Get a browserify / babel starter kit going, using clues from
      https://github.com/ngryman/gulp-bro
      https://babeljs.io/docs/plugins/preset-env/,
      http://2ality.com/2017/02/babel-preset-env.html,
      and a dash of
      https://github.com/makyo-dev/vue2-gulp-example/blob/master/gulpfile.js
    * Bundle all Vue code + toplevel into newsletter-admin{,.min}.js
    
    In WordPress glue:
    * With help from
      https://torquemag.io/2017/02/using-vuejs-components-wordpress-rest-api/
      and https://torquemag.io/2017/08/using-vuejs-es6-wordpress-admin/,
      serve the admin-
    
    In Vue code:
    * "Hello world" component (hello.vue) + top-level (composer.js)
    * Zero warnings in the browser's JS console

--- newsletter-theme/hook.php
+++ newsletter-theme/hook.php
@@ -19,3 +19,11 @@ add_action("epfl_newsletter_init", function() {
         "epfl-sti",
         dirname(__FILE__) . '/theme.php');
 });
+
+add_action("load-admin_page_newsletter_emails_new", function() {
+    $script_slug = "epfl_sti_newsletter_composer";
+    wp_register_script( $script_slug,
+                       get_theme_file_uri("/assets/newsletter-admin.min.js"),
+                       "0.1.0" );
+    wp_enqueue_script($script_slug);
+});
