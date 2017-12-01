543b0db7eae6576df0bd924f543f45e492fd0651
John Maxwell <john.maxwell@epfl.ch>
2017-12-01 15:35:35 +0100

    layout fine-tuning, banner reauthored with correct font : Trajan

--- newsletter-theme/theme.php
+++ newsletter-theme/theme.php
@@ -78,15 +78,15 @@ $date = "Vol. 1, No. 1, November 2017";
 		       	function redtitle($name) {
 			 return "<tr> <td style='font-family:verdana,sans-serif; background-color: #c50813; color:white; padding: 4px 0px 4px 8px; '>$name</td> </tr>";
 		       	} 
-                        function format_date($day,$month) {
-			       return "<table cellpadding=0 cellspacing=0 border=0 style='border:3px solid #aaa; background-color:#eee;'><td style='width: 36px; padding:0px 5px 0px 5px' align=center><span style='font-size:20px;'>$day</span><br><span style='font-weight: normal; color:#c50813; vertical-align:top;'><sup>$month</span></td></table>";
+                        function format_date($day,$month,$link) {
+			       return "<div style='border:3px solid #aaa; background-color:#eee; width: 36px; padding:0px 5px 0px 5px' align=center><a href=$link style='text-decoration:none; color:#000'><span style='font-size:20px;'>$day</span></a><br><span style='font-weight: bold; color:#c50813; vertical-align:top;'><sup>$month</span></div>";
 		        }
 			function format_event($title,$day,$month,$link,$place,$outlink) {
-			       $datebox=format_date($day,$month);
-			       return "<a href=$link><table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;' colspan=2>$title</td></tr><tr><td style='padding: 0px 0px 10px 0px;' width=50 align=left valign=top>$datebox</td><td style='font-size:12px;' align=right>$place<br>$outlink</td></tr><tr height=2><td colspan=2 height=2 style='background-color:#c50813; height:2px;'></td></tr></table></a>";
+			       $datebox=format_date($day,$month,$link);
+			       return "<table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;' colspan=2><a href=$link style='text-decoration:none; color:#000'>$title</a></td></tr><tr><td style='padding: 0px 0px 10px 0px;' width=50 align=left valign=top><a href=$link>$datebox</a></td><td style='font-size:12px;' align=right>$place<br><a href='$outlink' style='text-decoration:none; color:#000'>Add to calendar</a></td></tr><tr><td colspan=2 style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</td></tr></table>";
 		        }
 			function format_media($article,$link,$source,$date) {
-			       return "<a href=#><table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;'>$article</td></tr><tr><td style='font-size:10px;' align=right>$source, $date</td></tr><tr height=2><td colspan=2 height=2 style='background-color:#c50813; height:2px;'></td></tr></table></a>";
+			       return "<table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;'><a href=$link style='text-decoration:none; color:#000'>$article</a></td></tr><tr><td style='font-size:10px;' align=right>$source, $date</td></tr><tr><td style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</tr></table>";
 		        }
 			$whitebg="style='background-color:white;'";
 			$opentable="<table width=100% cellpadding=8 cellspacing=0 border=0>";
@@ -94,19 +94,19 @@ $date = "Vol. 1, No. 1, November 2017";
 			$righthand_column.=redtitle("EVENTS");
 			$righthand_column.="
 			 <tr>
-			  <td $whitebg>".format_event('Autonomous cars, intelligent cars','27','nov','#','EPFL Wallis','Add to calendar')." </td>
+			  <td $whitebg>".format_event('Autonomous cars, intelligent cars','27','nov','#','EPFL Wallis','#calendarlink')." </td>
 			 </tr>
 			 <tr>
 			  <td $whitebg>".format_event('Cutting diamonds to make optical components','28','nov','#','Forum Rolex','Add to calendar')."
 			  </td>
 			 </tr>
 			 <tr>
-			  <td $whitebg align=right><a href=#><table><td style='font-size:12px;'>More...</td></table></a> 
+			  <td $whitebg align=right><table><td><a href='https://sti.epfl.ch/seminars'  style='font-size:12px; text-decoration:none; color:#000'>More...</a></td></table>
 			  </td>
 			 </tr>
 			</table>
 		       ";
-		       $righthand_column.="<br><br>".$opentable;
+		       $righthand_column.="<br>".$opentable;
 		       $righthand_column.=redtitle("IN THE MEDIA");
 		       $righthand_column.="
 			 <tr>
@@ -115,6 +115,10 @@ $date = "Vol. 1, No. 1, November 2017";
 			 </tr>
 			 <tr>
 			  <td $whitebg>".format_media('National Radio covers the two European projects to be led by Tobias Kippenberg','#','RTS','20th November, 2017')."
+			  </td>
+			 </tr>
+			 <tr>
+			  <td $whitebg align=right><table><td><a href='https://sti.epfl.ch/seminars'  style='font-size:12px; text-decoration:none; color:#000'>More...</a></td></table>
 			  </td>
 			 </tr>
 			</table>
@@ -153,8 +157,8 @@ $date = "Vol. 1, No. 1, November 2017";
 			 }
                          echo "<tr> <td width=450 $colspan style='padding: 20px 10px 20px 10px; background-color:#fff; font-size: 13px; color: #666; font-family:Tahoma,Verdana,sans-serif'>";
                          $image_path = get_thumb_relpath(wp_get_attachment_metadata(get_post_thumbnail_id(get_the_id())));
-                          if ($image_path) {
-                         echo "<img hspace=8 src='$image_path' style='$imagesize' alt='picture' align='left'/>";
+                         if ($image_path) {
+                          echo "<img hspace=18 src='$image_path' style='$imagesize' alt='picture' align='left'/>";
                          }
                          echo "<p><a target='_tab' href='";
 			 echo get_permalink();  
@@ -176,7 +180,7 @@ $date = "Vol. 1, No. 1, November 2017";
 
                         endforeach; # posts 
                        endforeach; # categories 
-                         if (!isset($theme_options['theme_social_disable'])) { ?> 
+                         if (isset($theme_options['theme_social_disable'])) { ?> <!--- put "!" back before isset --->
                             <tr>
                                 <td  style="font-family: Tahoma,Verdana,sans-serif; font-size: 12px">
                                     <?php include WP_PLUGIN_DIR . '/newsletter/emails/themes/default/social.php'; ?>
@@ -185,14 +189,12 @@ $date = "Vol. 1, No. 1, November 2017";
                         <?php } ?>
                         <tr>
 			    <td colspan=2>
-			  	<table cellpadding=0 cellspacing=0 border=0 width=100%>
-			         <tr> <td style='height:10px; background-color: #c50813; width:100%'></td> </tr>
+			  	<table id='footer' cellpadding=0 cellspacing=0 border=0 width=100%>
 				 <tr>
-				  <td style="background-color:white;font-size:10px; font-family:verdana,sans-serif;padding: 5px;" align=center>
-                                   School of Engineering, &Eacute;cole Polytechnique F&eacute;d&eacute;rale de Lausanne (EPFL), Switzerland<br>
-				You can access previous versions of the newsletter here
+				  <td style="border-top:10px solid #c50813; background-color:white;font-size:10px; font-family:verdana,sans-serif;padding: 5px;" align=center>
+                                   <a style="color: #c50813; text-decoration: none; font-weight: bold" href="https://sti.epfl.ch">School of Engineering</a>, &Eacute;cole Polytechnique F&eacute;d&eacute;rale de Lausanne (<a style="color: #c50813; text-decoration: none; font-weight: bold" href="https://www.epfl.ch">EPFL</a>), Switzerland<br>You can access previous versions of the newsletter <a style="color: #c50813; text-decoration: none; font-weight: bold" href=#>here</a>
 <br>
-Unsubscribe by clicking <a target="_tab" href="{unsubscription_url}">here</a>
+Unsubscribe by clicking <a style="color: #c50813; text-decoration: none; font-weight: bold" target="_tab" href="{unsubscription_url}">here</a>
 				 </td>
 				 </tr>
 			        </table>
