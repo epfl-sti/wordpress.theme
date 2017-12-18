9e0ab003891a6175e1d3ebfe1f575215fee83327
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-18 23:36:06 +0100

    (WIP) Demo of saving the composer form

--- newsletter-theme/vue/Composer.vue
+++ newsletter-theme/vue/Composer.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
     <item-picker></item-picker>
+    <refresh-button :state="$data"></refresh-button>
   </div>
 </template>
 
 <script>
   import WPajax from "../inc/ajax.js"
   import ItemPicker from "./ItemPicker.vue"
+  import RefreshButton from "./RefreshButton.vue"
 
   export default {
     data () {
       return {
+        hero: undefined,
+        volumeNo: 1,
+        issueNo: 1,
+        news: [],
+        events: [],
+        openPositions: [],
+        inTheMedia: []
       }
     },
     components: {
-        'item-picker': ItemPicker
+        'item-picker': ItemPicker,
+        'refresh-button': RefreshButton
     }
   }
 </script>
