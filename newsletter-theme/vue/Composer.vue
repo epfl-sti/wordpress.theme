06b53e1d1fefddb69785f6df80604b184969274b
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-18 23:35:14 +0100

    Un-helloworld-ify the things

--- newsletter-theme/vue/Composer.vue
+++ newsletter-theme/vue/Composer.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <item-picker></item-picker>
-    <p>{{ greeting }} World!</p>
-    <input type="text" v-model="number" @change="numberChanged"/>
-    <p>+ 10 = {{ plusten }}</p>
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors">
-        {{error.message}}
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -19,29 +11,10 @@
   export default {
     data () {
       return {
-        greeting: "hello",
-        number: 0,
-        plusten: 0,
-        errors: []
       }
     },
     components: {
         'item-picker': ItemPicker
-    },
-    methods: {
-      numberChanged () {
-        var self = this;
-        WPajax({
-          "action": "epfl_sti_newsletter_addten",
-          "number": this.number
-        })
-        .then(response => {
-          self.plusten = response.number
-        })
-        .catch(e => {
-          self.errors.push(e)
-        })
-      }
     }
   }
 </script>
