f6b1701186fae51ad821cf6ef28b1e5183fddd2b
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-19 07:50:35 +0100

    Refactor theme.php
    * More functions
    * More CSS
    * Permanently get rid of the social bar

--- newsletter-theme/theme.php
+++ newsletter-theme/theme.php
@@ -33,11 +33,169 @@ p {
  padding: 0px;
  margin: 0px;
 }
+
+a {
+ text-decoration: none;
+ color: black;
+}
+
+a.outlink.more {
+    font-size:12px;
+}
+
+table.righthand-column td {
+    background-color:white;
+}
+
+.redtitle {
+    text-align: inherit;
+    font-family: verdana,sans-serif;
+    font-weight: inherit;
+    color:white;
+    background-color: #c50813;
+    padding: 4px 0px 4px 8px;
+}
+
+.date {
+    border:3px solid #aaa;
+    background-color:#eee;
+    width: 36px;
+    padding:0px 5px 0px 5px;
+    text-align: center;
+}
+
+.date .day {
+    font-size:20px;
+}
+
+.date .month {
+    font-weight: bold;
+    color:#c50813;
+    vertical-align:top;
+}
+
+.newstitle {
+    font-size: 14px;
+    font-family:Tahoma,Verdana,sans-serif;
+}
+
+.newstitle.hero {
+    font-size: 18px;
+}
+
+#footer td {
+    border-top:10px solid #c50813;
+    background-color:white;
+    padding: 5px;
+    text-align: center;
+    font-size:10px;
+    font-family:verdana,sans-serif
+}
+
+#footer a {
+    color: #c50813;
+    text-decoration: none;
+    font-weight: bold;
+}
+
 </style>
 <?php }
 
-// <table>s everywhere is the way to go - Not sure how ancient versions of Outlook
-// like HTML5 stuff.
+
+// <table>s everywhere is the way to go - Ancient versions of Outlook
+// probably don't like HTML5 stuff much.
+
+function render_red_title ($name)
+{
+    echo "<tr><th class=\"redtitle\">$name</th> </tr>";
+}
+
+function render_media ($article, $link, $source, $date)
+{
+    echo "<table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;'><a href=\"$link\">$article</a></td></tr><tr><td style='font-size:10px;' align=right>$source, $date</td></tr><tr><td style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</tr></table>";
+}
+
+function render_event ($title, $day, $month, $link, $place, $outlink)
+{
+    $datebox = "<div class=\"date\"><a href=\"$link\"><span class=\"day\">$day</span><br><span class=\"month\">$month</span></a></div>";
+    ?>
+    <table><tr><td style="font-size:14px; width:100%; padding: 0px 0px 10px 0px;" colspan=2><a href="<?php echo $link; ?>"><?php echo $title; ?></a></td></tr><tr><td style='padding: 0px 0px 10px 0px;' width=50 align=left valign=top><a href="<?php echo $link; ?>"><?php echo $datebox; ?></a></td><td style="font-size:12px;" align=right><?php echo $place; ?><br><a href="<?php echo $outlink; ?>">Add to calendar</a></td></tr><tr><td colspan=2 style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</td></tr></table>
+    <?php
+}
+
+function render_righthand_column ()
+{
+    $opentable = "<table class=\"righthand-column\" width=\"100%\" cellpadding=\"8\" cellspacing=\"0 border=\"0\">";
+			echo "$opentable";
+			render_red_title("EVENTS");
+			echo "
+			 <tr>
+			  <td>";
+                render_event('Towards next-generation membranes for energy-efficient molecular separation','7','dec','http://sti.epfl.ch/page-42995.html#httpsmementoepflchimage10622122x122jpg','EPFL Wallis (Sion)','https://stisrv13.epfl.ch/outlink.php?enddate=20171207T113000&datestring=20171207T103000&speaker=Prof.%20Kumar%20AgrawalLaboratory%20of%20advanced%20separationsEPFL%20Valais,%20Sion&title=Towards%20the%20next-generation%20membranes%20for%20energy-efficient%20molecular%20separation&room=Zeuzier,%20I17%204%20K2');
+                echo " </td>
+			 </tr>
+			 <tr>
+			  <td>";
+                render_event('What impact can integrated photonics have on data center architecture?','8','dec','http://sti.epfl.ch/page-42995.html#httpsmementoepflchimage10667122x122jpg','BC 420','https://stisrv13.epfl.ch/outlink.php?enddate=20171208T120000&datestring=20171208T110000&speaker=S%E9bastien%20Rumley,%20Research%20Scientist%20in%20the%20Lightwave%20Research%20Laboratory,%20Columbia%20University,%20New%20York&title=What%20impact%20can%20Integrated%20Photonics%20have%20on%20data%20center%20architecture?&room=BC%20420');
+                echo "
+			  </td>
+			 </tr>
+			 <tr>
+			  <td align=right><table><td><a href=\"https://sti.epfl.ch/seminars\" class=\"outlink more\">More...</a></td></table>
+			  </td>
+			 </tr>
+			</table>
+		       ";
+		       echo "<br>$opentable";
+		       render_red_title("IN THE MEDIA");
+		       echo "
+			 <tr>
+			  <td>";
+              render_media('Sylvie Roke makes waves with her work on 3D imaging of surface chemistry','https://www.eurekalert.org/pub_releases/2017-07/epfd-3io071917.php','Science News','September 2017');
+              echo "
+			  </td>
+			 </tr>
+			 <tr>
+			  <td>";
+              render_media('Elison Matioli makes groundbreaking use of slanted tri-gate structures','http://www.semiconductor-today.com/news_items/2017/aug/epf_230817.shtml','Semiconductor Today','August, 2017');
+              echo "
+			  </td>
+			 </tr>
+			 <tr>
+			  <td align=right><table><td><a href=\"https://sti.epfl.ch/news\" class=\"outlink more\">More...</a></td></table>
+			  </td>
+			 </tr>
+			</table>
+		       ";
+}
+
+function render_news_item ($style) {
+    if ($style === "hero") {
+        $colspan="colspan=2"; #first article only
+    } else {
+        $colspan="colspan=1";	
+    }
+    if ($style === "hero" or $style === "large") {
+        $rowstyle="' class=\"newstitle $style\">";
+        $imagesize="width:250px";
+    } else {
+        $rowstyle="' class=\"newstitle\">";
+        $imagesize="";
+  }
+
+    echo "<td width=450 $colspan style='padding: 20px 10px 20px 10px; background-color:#fff; font-size: 13px; color: #666; font-family:Tahoma,Verdana,sans-serif'>";
+    $image_path = get_thumb_relpath(wp_get_attachment_metadata(get_post_thumbnail_id(get_the_id())));
+    if ($image_path) {
+        echo "<img hspace=18 src='$image_path' style='$imagesize' alt='picture' align='left'/>";
+    }
+    echo "<p><a target='_tab' href='";
+    echo get_permalink();  
+    echo $rowstyle; 
+    echo the_title(); 
+    echo"</a></p>";
+    the_excerpt(); 
+    echo "</td>";
+}
 
 $date = "Vol. 1, No. 1, December 2017";
 
@@ -75,54 +233,6 @@ $date = "Vol. 1, No. 1, December 2017";
 					<img src="<?php echo get_theme_relative_uri() . "/newsletter-theme/feature.png"; ?>"/></td>
 			</tr>
                        <?php 
-		       	function redtitle($name) {
-			 return "<tr> <td style='font-family:verdana,sans-serif; background-color: #c50813; color:white; padding: 4px 0px 4px 8px; '>$name</td> </tr>";
-		       	} 
-                        function format_date($day,$month,$link) {
-			       return "<div style='border:3px solid #aaa; background-color:#eee; width: 36px; padding:0px 5px 0px 5px' align=center><a href=$link style='text-decoration:none; color:#000'><span style='font-size:20px;'>$day</span></a><br><span style='font-weight: bold; color:#c50813; vertical-align:top;'><sup>$month</span></div>";
-		        }
-			function format_event($title,$day,$month,$link,$place,$outlink) {
-			       $datebox=format_date($day,$month,$link);
-			       return "<table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;' colspan=2><a href=$link style='text-decoration:none; color:#000'>$title</a></td></tr><tr><td style='padding: 0px 0px 10px 0px;' width=50 align=left valign=top><a href=$link>$datebox</a></td><td style='font-size:12px;' align=right>$place<br><a href='$outlink' style='text-decoration:none; color:#000'>Add to calendar</a></td></tr><tr><td colspan=2 style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</td></tr></table>";
-		        }
-			function format_media($article,$link,$source,$date) {
-			       return "<table><tr><td style='font-size:14px; width:100%; padding: 0px 0px 10px 0px;'><a href=$link style='text-decoration:none; color:#000'>$article</a></td></tr><tr><td style='font-size:10px;' align=right>$source, $date</td></tr><tr><td style='border-bottom:2px solid #c50813; font-size:1px'>&nbsp;</tr></table>";
-		        }
-			$whitebg="style='background-color:white;'";
-			$opentable="<table width=100% cellpadding=8 cellspacing=0 border=0>";
-		       	$righthand_column=$opentable;
-			$righthand_column.=redtitle("EVENTS");
-			$righthand_column.="
-			 <tr>
-			  <td $whitebg>".format_event('Towards next-generation membranes for energy-efficient molecular separation','7','dec','http://sti.epfl.ch/page-42995.html#httpsmementoepflchimage10622122x122jpg','EPFL Wallis (Sion)','https://stisrv13.epfl.ch/outlink.php?enddate=20171207T113000&datestring=20171207T103000&speaker=Prof.%20Kumar%20AgrawalLaboratory%20of%20advanced%20separationsEPFL%20Valais,%20Sion&title=Towards%20the%20next-generation%20membranes%20for%20energy-efficient%20molecular%20separation&room=Zeuzier,%20I17%204%20K2')." </td>
-			 </tr>
-			 <tr>
-			  <td $whitebg>".format_event('What impact can integrated photonics have on data center architecture?','8','dec','http://sti.epfl.ch/page-42995.html#httpsmementoepflchimage10667122x122jpg','BC 420','https://stisrv13.epfl.ch/outlink.php?enddate=20171208T120000&datestring=20171208T110000&speaker=S%E9bastien%20Rumley,%20Research%20Scientist%20in%20the%20Lightwave%20Research%20Laboratory,%20Columbia%20University,%20New%20York&title=What%20impact%20can%20Integrated%20Photonics%20have%20on%20data%20center%20architecture?&room=BC%20420')."
-			  </td>
-			 </tr>
-			 <tr>
-			  <td $whitebg align=right><table><td><a href='https://sti.epfl.ch/seminars'  style='font-size:12px; text-decoration:none; color:#000'>More...</a></td></table>
-			  </td>
-			 </tr>
-			</table>
-		       ";
-		       $righthand_column.="<br>".$opentable;
-		       $righthand_column.=redtitle("IN THE MEDIA");
-		       $righthand_column.="
-			 <tr>
-			  <td $whitebg>".format_media('Sylvie Roke makes waves with her work on 3D imaging of surface chemistry','https://www.eurekalert.org/pub_releases/2017-07/epfd-3io071917.php','Science News','September 2017')."
-			  </td>
-			 </tr>
-			 <tr>
-			  <td $whitebg>".format_media('Elison Matioli make groundbreaking use of slanted tri-gate structures','http://www.semiconductor-today.com/news_items/2017/aug/epf_230817.shtml','Semiconductor Today','August, 2017')."
-			  </td>
-			 </tr>
-			 <tr>
-			  <td $whitebg align=right><table><td><a href='https://sti.epfl.ch/news'  style='font-size:12px; text-decoration:none; color:#000'>More...</a></td></table>
-			  </td>
-			 </tr>
-			</table>
-		       ";
 		       $index = 0;
 		       $count = 0;
                        foreach (get_newsletter_categories($theme_options) as $cat):
@@ -139,37 +249,19 @@ $date = "Vol. 1, No. 1, December 2017";
                          // The theme can "suggest" a subject replacing the one configured, for example. In this case
                          // the theme, is there is no subject, suggest the first post title.
                          if (empty($theme_options['subject'])) $theme_options['subject'] = $post->post_title;
-			 if (($count<1)||($count>3)){
-			  if ($count<1) {
-				 $colspan="colspan=2"; #first article only
-		   	  }
-			  $rowstyle="' style='font-size: 18px; color: #000; text-decoration: none;font-family:Tahoma,Verdana,sans-serif'>";
-			  $imagesize="width:250px";
-  			 }
-  			 else {
-			  $colspan="colspan=1";	
-			  $rowstyle="' style='font-size: 14px; color: #000; text-decoration: none;font-family:Tahoma,Verdana,sans-serif'>";
-			  $imagesize="";
-			 }
 			 if ($count>3) {
 			  echo "<tr><td><table cellpadding=0 cellspacing=0 border=0>";
-			  echo redtitle("OPEN POSITIONS");
+			  render_red_title("OPEN POSITIONS");
 			 }
-                         echo "<tr> <td width=450 $colspan style='padding: 20px 10px 20px 10px; background-color:#fff; font-size: 13px; color: #666; font-family:Tahoma,Verdana,sans-serif'>";
-                         $image_path = get_thumb_relpath(wp_get_attachment_metadata(get_post_thumbnail_id(get_the_id())));
-                         if ($image_path) {
-                          echo "<img hspace=18 src='$image_path' style='$imagesize' alt='picture' align='left'/>";
-                         }
-                         echo "<p><a target='_tab' href='";
-			 echo get_permalink();  
-			 echo $rowstyle; 
-			 echo the_title(); 
-			 echo"</a></p>";
-                         the_excerpt(); 
-                         echo "</td>";
-                         if ($count === 1) { 
-			  echo "<td rowspan=4 valign=top style='padding: 0px; background-color:#d6d6d6; font-size: 14px; color: #666; font-family:Tahoma,Verdana,sans-serif'>$righthand_column</td>";
-			 }
+             echo "<tr>";
+             render_news_item($count === 0 ? "hero" : "normal");
+             if ($count === 1) {
+                 ?>
+                 <td rowspan=4 valign=top style="padding: 0px; background-color:#d6d6d6; font-size: 14px; color: #666; font-family:Tahoma,Verdana,sans-serif">
+                   <?php render_righthand_column() ?>
+                 </td>
+                 <?php
+             }
 			 echo " </tr>";
 			 if ($count>3) {
 			  echo "</table></td></tr>";
@@ -180,24 +272,18 @@ $date = "Vol. 1, No. 1, December 2017";
 
                         endforeach; # posts 
                        endforeach; # categories 
-                         if (isset($theme_options['theme_social_disable'])) { ?> <!--- put "!" back before isset --->
-                            <tr>
-                                <td  style="font-family: Tahoma,Verdana,sans-serif; font-size: 12px">
-                                    <?php include WP_PLUGIN_DIR . '/newsletter/emails/themes/default/social.php'; ?>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        <tr>
+            ?>
+            <tr>
 			    <td colspan=2>
-			  	<table id='footer' cellpadding=0 cellspacing=0 border=0 width=100%>
+			    <table id='footer' cellpadding=0 cellspacing=0 border=0 width=100%>
 				 <tr>
-				  <td style="border-top:10px solid #c50813; background-color:white;font-size:10px; font-family:verdana,sans-serif;padding: 5px;" align=center>
-                                   <a style="color: #c50813; text-decoration: none; font-weight: bold" href="https://sti.epfl.ch">School of Engineering</a>, &Eacute;cole Polytechnique F&eacute;d&eacute;rale de Lausanne (<a style="color: #c50813; text-decoration: none; font-weight: bold" href="https://www.epfl.ch">EPFL</a>), Switzerland<br>You can access previous versions of the newsletter <a style="color: #c50813; text-decoration: none; font-weight: bold" href=#>here</a>
+				  <td>
+                         <a href="https://sti.epfl.ch">School of Engineering</a>, &Eacute;cole Polytechnique F&eacute;d&eacute;rale de Lausanne (<a href="https://www.epfl.ch">EPFL</a>), Switzerland<br>You can access previous versions of the newsletter <a href="#">here</a>
 <br>
-Unsubscribe by clicking <a style="color: #c50813; text-decoration: none; font-weight: bold" target="_tab" href="{unsubscription_url}">here</a>
+Unsubscribe by clicking <a target="_tab" href="{unsubscription_url}">here</a>
 				 </td>
 				 </tr>
-			        </table>
+			    </table>
                             </td>
                         </tr>
                     </table>
