0d1c3619d21f5143d0fc543399d35fc26f6651bc
Nicolas Borboën <nicolas.borboen@epfl.ch>
2017-12-29 00:43:49 +0100

    Footer updated with social icons
    * Big clean up of `sidebar-footerfull.php` (why the hell our footer is
    in a sidebar template?);
    * Footer contents back in `footer.php`;
    * Twitter, Instagra, Google Plus, Youtube and Facebook added.

--- sidebar-footerfull.php
+++ sidebar-footerfull.php
@@ -5,12 +5,14 @@
  * @package epflsti
  */
 
-$container   = get_theme_mod( 'epflsti_container_type' );
+$container   = get_theme_mod('epflsti_container_type');
 
 ?>
 
 <?php if ( is_active_sidebar( 'footerfull' ) ) : ?>
+
 	<!-- ******************* The Footer Full-width Widget Area ******************* -->
+
 	<div class="wrapper" id="wrapper-footer-full">
 
 		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
@@ -24,28 +26,5 @@ $container   = get_theme_mod( 'epflsti_container_type' );
 		</div>
 
 	</div><!-- #wrapper-footer-full -->
-<?php else:    # Default footer is a map and contact info ?>
-
-<div class="footer">
-   <div class=footercontainer>
-   <span class=footertitle>CONTACT</span><br><br>
-School of Engineering<br>
-Dean's Office<br>
-EPFL - ELB 114<br>
-Station 11<br>
-CH-1015 Lausanne<br><br>
-email: <a href="mailto:secretariat.sti@epfl.ch">STI Secretary Office</a>
-  </div>
-  <script src="<?= get_template_directory_uri() . '/js/google-maps.js' ?>"></script>
-  <div id="googlemap"></div>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-KB1byyUR6AEWVI1B8cdGFIDI1v8g8YY&libraries=places&callback=initMap" async defer></script>
-
-   <div class=footercontainer>
-   <span class=footertitle>NEWSLETTER</span><br><br>
-   Sign up for our email bulletin:
-   <form><input name=subscriber><br><br><input type=submit value=GO></form>
-    
-   </div>
- </div> <!-- footer --->
 
 <?php endif; ?>
