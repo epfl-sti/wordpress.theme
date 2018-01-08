477fc4d3558f961115797c123c5bb4c5aab6e3c2
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-08 17:43:02 +0100

    Render videos as their own full self
    This lets e.g. https://sti-test.epfl.ch/category/student-testimonies/
    Just Work.

---
+++ loop-templates/content-video.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Loop template invoked for posts that have "format" set to "video".
+ *
+ * For instance,
+ * https://sti-test.epfl.ch/fr/category/temoignages-detudiants/ shows these.
+ */
+
+the_title( '<h1 class="entry-title">', '</h1>' );
+the_content();
+
