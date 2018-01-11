37777d30abf2945cd2a1bf624d3edf812f53e5f6
John Maxwell <john.maxwell@epfl.ch>
2018-01-11 10:57:40 +0100

    progress on people page

--- loop-templates/content-single-epfl-person.php
+++ loop-templates/content-single-epfl-person.php
@@ -20,7 +20,7 @@ $firstname="Volkan";
 $epflname="volkan.cevher";
 $phone="+41 21 693 1101";
 $office="ELE233";
-
+$sciper="199128";
 
 $epfl_positions="
 <br><br>
@@ -56,6 +56,50 @@ $newsimage3="https://stisrv13.epfl.ch/newsdesk/covershots/el/left1180.png";
 $newslink1="http://sti.epfl.ch/page-140428.html#anchor2017";
 $newslink2="http://sti.epfl.ch/page-67196.html#anchor1263";
 $newslink3="http://sti.epfl.ch/page-57268.html";
+
+$publicationtext1="C. Aprile, A. Cevrero, P. A. Francese, C. Menolfi and M. Braendli et al. An Eight lanes 7Gb/s/pin Source Synchronous Single-Ended RX with Equalization and Far-End Crosstalk Cancellation for Backplane Channels, in IEEE Journal of Solid State Circuits, vol. PP, num. 99, p. 1-12, 2018";
+$publicationlink1="https://infoscience.epfl.ch/record/233712/files/08246724.pdf?version=1";
+$publicationrecord1="https://infoscience.epfl.ch/record/233712?ln=en";
+
+$publicationtext2="S. Mitrovic, I. Bogunovic, A. Norouzi Fard, J. Tarnawski and V. Cevher. Streaming Robust Submodular Maximization: A Partitioned Thresholding Approach. Conference on Neural Information Processing Systems (NIPS), Long Beach, 2017";
+$publicationlink2="https://infoscience.epfl.ch/record/232540/files";
+$publicationrecord2="https://infoscience.epfl.ch/record/232540?ln=en";
+
+$publicationtext3="A. Alacaoglu, Q. Tran-Dinh, O. Fercoq and V. Cevher. Smooth Primal-Dual Coordinate Descent Algorithms for Nonsmooth Convex Optimization. 31st Conference on Neural Information Processing Systems (NIPS 2017), Long Beach, CA, USA, 2017";
+$publicationlink3="https://infoscience.epfl.ch/record/232391/files/SMOOTH-CD_MAIN.pdf?version=1";
+$publicationrecord3="https://infoscience.epfl.ch/record/232391?ln=en";
+
+$publicationtext4="I. Bogunovic, S. Mitrovic, J. Scarlett and V. Cevher. A Distributed Algorithm for Partitioned Robust Submodular Maximization. IEEE International Workshop on Computational Advances in Multi-Sensor Adaptive Processing (CAMSAP), 2017";
+$publicationlink4="https://infoscience.epfl.ch/record/232383/files/A%20Distributed%20Algorithm%20for%20Partitioned%20Robust%20Submodular%20Maximization.pdf?version=1";
+$publicationrecord4="https://infoscience.epfl.ch/record/232383?ln=en";
+
+$fullcontact="
+<table>
+ <tr>
+  <td><h1><a href=https://people.epfl.ch/$epflname>$firstname $surname</a></h1><br></td>
+ </tr>
+ <tr>
+  <td colspan=2>Associate Professor</td>
+ </tr>
+ <tr>
+  <td colspan=2><a href=$labwebsite>$mylabname ($labname)</a><br><br></td>
+ </tr>
+ <tr>
+  <td>Phone:</td><td><a href=tel:'$phone'>$phone</a></td>
+ </tr>
+ <tr>
+  <td></td><td><a href=#>WebCall <img src=https://people.epfl.ch/images/info.png></a></td>
+ </tr>
+ <tr>
+  <td>Postal address</td><td valign=top>EPFL STI IEL LIONS<br>
+				ELE 233 (Bâtiment ELE)<br>
+				Station 11<br>
+				CH-1015 Lausanne<br>
+				Office	ELE 233</td>
+ </tr>
+</table> 
+
+";
 ?>
 
 
@@ -71,7 +115,7 @@ $newslink3="http://sti.epfl.ch/page-57268.html";
   <div class="col-sm-8 content-area" id="primary" style="">
    <main class="site-main sti_content_maincolumn" id="main" role="main">
     <header class="entry-header">
-     <div class="sti_content_title"><?php the_title(); ?></div>
+     <h1><?php the_title(); ?></h1>
     </header><!-- .entry-header -->
 
     <div class="entry-content holding">
@@ -80,21 +124,15 @@ $newslink3="http://sti.epfl.ch/page-57268.html";
      </div>
      <div class="sti_content_prof_text">
 		<?php //the_content(); ?>
-<p style="width: 400px" ><?php echo "$position<br><br>$bio"; ?></p><?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
-			'after'  => '</div>',
-		) );
-		?>
+                <p style="width: 400px" ><?php echo "$position<br><br>$bio"; ?></p>
      </div>
     </div><!-- .entry-content -->
    </main><!-- #main -->
    <br>
    <main class="site-main sti_content_maincolumn" id="main" role="main">
-
-    <div class="entry-content holding">
+    <div class="entry-content">
      <div class="sti_content_prof_text">
-     <p style="width: 400px" ><?php echo $epfl_positions ?></p>
+      <?php echo $epfl_positions ?>
      </div>
     </div><!-- .entry-content -->
    </main><!-- #main -->
@@ -111,11 +149,10 @@ $newslink3="http://sti.epfl.ch/page-57268.html";
     <div class="menu-ibi-nav-menu-container">
      <ul id="menu-ibi-nav-menu" class="menu">
       <li id="menu-item-128" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-128"><a href="<?php echo $labwebsite; ?>">LAB WEBSITE</a></li>
-	<li id="menu-item-129" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-129"><a href="#">PUBLICATIONS</a></li>
-	<li id="menu-item-131" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-131"><a href="#">NEWS</a></li>
-	<li id="menu-item-132" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-132"><a href="#">COURSES</a></li>
-	<li id="menu-item-130" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-130"><a href="#">COLLABORATIONS</a></li>
-	<li id="menu-item-132" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-132"><a href="#">FULL CONTACT DETAILS</a></li>
+      <li id="menu-item-129" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-129"><a href="https://people.epfl.ch/cgi-bin/people?id=<?php echo $sciper;?>&op=publications&lang=en&cvlang=en">PUBLICATIONS</a></li>
+	<li id="menu-item-131" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-131"><a href="#news">NEWS</a></li>
+	<li id="menu-item-130" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-130"><a href="https://stisrv13.epfl.ch/collaborations/tube_html5.php?sciper=<?php echo $sciper; ?>&showpublications=1&showpatents=1&showexternals=1&showindustry=1">COLLABORATIONS</a></li>
+	<li id="menu-item-132" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-132"><a href="#fullcontact">FULL CONTACT DETAILS</a></li>
      </ul>
      <div class="sti_people_menu_white">
      <?php 
@@ -125,18 +162,21 @@ echo "$firstname $surname<br>$position<br>Office: <a class=whitelink href=https:
     </div>
    </div>
   </div><!-- .sti_righthand_menu -->
-  <!-- NAV MENU END -->
  </div><!-- .row -->
+  <!-- NAV MENU END -->
  <div class="row">
   <div class="col-md-8 content-area" id="primary">
    <main class="site-main sti_content_maincolumn" id="main" role="main">
-    <div class="entry-content holding">
+    <div class="entry-content">
      <div class="sti_content_prof_text">
-	NEWS<br><br>	
+	<h1>Recent Publications</h1>	
 <?php
-		echo "<a href=$newslink1><img src=$newsimage1><br>$newstitle1</a>";
-		echo "<a href=$newslink2><img src=$newsimage2><br>$newstitle2</a>"; 
-		echo "<a href=$newslink3><img src=$newsimage3><br>$newstitle3</a>"; 
+		echo "<h2>2018</h2>";
+		echo "<a href=$publicationlink1>$publicationtext1</a><br><br><a href=$publicationrecord1>Detailed record</a><br><br>";
+		echo "<h2>2017</h2>";
+		echo "<a href=$publicationlink2>$publicationtext2</a><br><br><a href=$publicationrecord2>Detailed record</a><br><br>";
+		echo "<a href=$publicationlink3>$publicationtext3</a><br><br><a href=$publicationrecord3>Detailed record</a><br><br>";
+		echo "<a href=$publicationlink4>$publicationtext4</a><br><br><a href=$publicationrecord4>Detailed record</a><br><br>";
 ?>
 
      </div>
@@ -144,10 +184,52 @@ echo "$firstname $surname<br>$position<br>Office: <a class=whitelink href=https:
    </main><!-- #main -->
   </div><!-- #primary -->
 
+ </div><!-- .row -->
+ <div class="row">
+  <div class="col-md-8 content-area" id="primary">
+   <main class="site-main sti_content_maincolumn" id="main" role="main">
+    <div class="entry-content holding">
+     <div class="sti_content_prof_text">
+	<a name=news></a>
+	<h1>News</h1>	
+      <div class="frontrowcontent">
+<?php
+		echo "<div class='sti_people_news' style='background-image:url(\"$newsimage1\");'><div class=peoplenewstitle><a class=whitelink href=$newslink1>$newstitle1</a></div></div>";
+		echo "<div class='sti_people_news' style='background-image:url(\"$newsimage2\");'><div class=peoplenewstitle><a class=whitelink href=$newslink2>$newstitle2</a></div></div>"; 
+		echo "<div class='sti_people_news' style='background-image:url(\"$newsimage3\");'><div class=peoplenewstitle><a class=whitelink href=$newslink3>$newstitle3</a></div></div>"; 
+		echo "<div class='sti_people_news' style='background-image:url(\"$newsimage1\");'><div class=peoplenewstitle><a class=whitelink href=$newslink1>$newstitle1</a></div></div>";
+?>
+
+      </div>
+     </div>
+    </div><!-- .entry-content -->
+   </main><!-- #main -->
+  </div><!-- #primary -->
+ </div><!-- .row -->
+
+ <div class="row">
+  <div class="col-md-8 content-area" id="primary">
+   <main class="site-main sti_content_maincolumn" id="main" role="main">
+    <div class="entry-content holding">
+     <div class="sti_content_prof_text">
+      <br><a name=fullcontact></a>
+       <?php echo $fullcontact; ?>
+      </div>
+     </div>
+    </div><!-- .entry-content -->
+   </main><!-- #main -->
+  </div><!-- #primary -->
+ </div><!-- .row -->
 </div><!-- Container end -->
 
 
-			<?php epflsti_posted_on(); ?>
+			<?php //epflsti_posted_on(); ?>
+		<?php
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
+			'after'  => '</div>',
+		) );
+		?>
 
 
 <footer class="entry-footer">
