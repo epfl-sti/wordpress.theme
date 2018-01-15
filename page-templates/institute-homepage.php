c98e4d92689d9cd58a45c4895e5931c0db56c588
Nicolas Borboën <nicolas.borboen@epfl.ch>
2018-01-15 21:42:11 +0100

    Add the container to align the content to the frontrow

--- page-templates/institute-homepage.php
+++ page-templates/institute-homepage.php
@@ -18,49 +18,53 @@ use function \EPFL\STI\get_institute;
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content">
 
-		<div class="row">
+		<div class="container">
 
-			<div class="col-md-9 content-area" id="primary">
+			<div class="row">
 
-				<main class="site-main" id="main" role="main">
+				<div class="col-md-9 content-area" id="primary">
 
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-						?>
-					<?php endwhile; // end of the loop. ?>
+					<main class="site-main" id="main" role="main">
 
-				</main><!-- #main -->
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+							<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+							?>
+						<?php endwhile; // end of the loop. ?>
 
-			</div><!-- #primary -->
+					</main><!-- #main -->
 
-			<?php dynamic_sidebar( 'right' ); ?>
+				</div><!-- #primary -->
 
-			<!-- NAV MENU START -->
-			<div class="institute-righthand-menu col-md-3">
-				<div class="institute-righthand-menu-title">
-					<?php
-					global $post;
-					?>
-					<a class="institute-righthand-menu-title-link" href="#"><?php echo strtoupper(get_institute()); ?></a>
-				</div>
-				<div class="institute-righthand-menu-list-container">
-					<?php wp_nav_menu( array(
-                        'theme_location' => 'institute-menu-'.get_institute(),
-                        'container_class' => sprintf(
-                            'institute-nav-menu %s%s',
-                            get_institute(),
-                            function_exists('pll_current_language') ?
-                              ' lang-' . pll_current_language() : '')
-                    ) ); ?>
-				</div>
-			</div><!-- .sti_righthand_menu -->
-			<!-- NAV MENU END -->
-		</div><!-- .row -->
+				<?php dynamic_sidebar( 'right' ); ?>
+
+				<!-- NAV MENU START -->
+				<div class="institute-righthand-menu col-md-3">
+					<div class="institute-righthand-menu-title">
+						<?php global $post; ?>
+						<a class="institute-righthand-menu-title-link" href="#"><?php echo strtoupper( get_institute() ); ?></a>
+					</div>
+					<div class="institute-righthand-menu-list-container">
+						<?php wp_nav_menu( array(
+																			'theme_location' => 'institute-menu-'.get_institute(),
+																			'container_class' => sprintf(
+																					'institute-nav-menu %s%s',
+																					get_institute(),
+																					function_exists('pll_current_language') ?
+																						' lang-' . pll_current_language() : '')
+																	) ); ?>
+					</div>
+				</div><!-- .sti_righthand_menu -->
+				<!-- NAV MENU END -->
+
+			</div><!-- .row -->
+
+		</div><!-- .container -->
+
 		<?php dynamic_sidebar( get_institute() . '-homepage' ); ?>
 
 	</div><!-- Container end -->
