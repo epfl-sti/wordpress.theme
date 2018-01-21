d05fbb7361bce074834f4eaa0e113d32b6af7320
Nicolas Borboën <nicolas.borboen@epfl.ch>
2018-01-21 11:21:38 +0100

    Allow to personalize Contact widget's title
    * form & update funciton implemented
    * $before_title and $after_title in order to personalize the contact
    widget call.

--- widgets/contact.php
+++ widgets/contact.php
@@ -12,6 +12,7 @@ if (! class_exists('WP_Widget')) {
 
 require_once(dirname(dirname(__FILE__)) . "/inc/i18n.php");
 use function \EPFL\STI\Theme\___;
+use function \EPFL\STI\Theme\__x;
 
 class Contact extends \WP_Widget
 {
@@ -26,13 +27,43 @@ class Contact extends \WP_Widget
     );
   }
 
-  public function widget($args, $config)
+  // The widget form (for the backend )
+  function form($instance) {
+    // Check values
+    if( $instance) {
+      $title = esc_attr($instance['title']);
+    } else {
+      $title = '';
+    }
+    print vsprintf("<p><label for=\"%s\">%s<input class=\"widefat\" id=\"%s\" name=\"%s\" type=\"text\" value=\"%s\" /></label></p>",
+                    array(
+                      $this->get_field_id('title'),
+                      __x('Title:', 'epfl_sti'),
+                      $this->get_field_id('title'),
+                      $this->get_field_name('title'),
+                      $title)
+                    );
+  }
+
+  // Update widget settings
+  public function update( $new_instance, $old_instance )
   {
+    $instance = $old_instance;
+    $instance['title'] = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
+    return $instance;
+  }
+
+  // Display the widget
+  public function widget( $args, $instance )
+  {
+    extract( $args );
+    $title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
+
     echo $args['before_widget'];
     ?>
     <div class="widget epfl-sti-social">
       <div itemscope itemtype="https://schema.org/ContactPoint">
-        <h1><?php echo ___('CONTACT'); ?></h1>
+        <h1><?php echo $before_title . $title . $after_title; ?></h1>
         <address>
           <div itemscope itemtype="schema.org/PostalAddress">
             <strong><span property="name"><?php echo ___('School of Engineering'); ?></span></strong><br />
