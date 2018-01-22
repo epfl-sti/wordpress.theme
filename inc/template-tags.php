6b523a09d7bf37ffb6b7bdae2ac2c196aa7d6ea6
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-22 14:55:28 +0100

    Make epflsti_entry_footer a lot more sober

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -35,24 +35,6 @@ if ( ! function_exists( 'epflsti_entry_footer' ) ) :
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function epflsti_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'epflsti' ) );
-		if ( $categories_list && epflsti_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'epflsti' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'epflsti' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'epflsti' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-		}
-	}
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'epflsti' ), esc_html__( '1 Comment', 'epflsti' ), esc_html__( '% Comments', 'epflsti' ) );
-		echo '</span>';
-	}
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
