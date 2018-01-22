960553b5e787d7e24d2bdf62f31db1945b155d5e
Nicolas Borboën <nicolas.borboen@epfl.ch>
2018-01-22 21:55:58 +0100

    refactor(syntax): Untabify

--- widgets/google-map.php
+++ widgets/google-map.php
@@ -7,7 +7,7 @@
 namespace EPFL\STI\Theme\Widgets;
 
 if (! class_exists('WP_Widget')) {
-    die( 'Access denied.' );
+  die( 'Access denied.' );
 }
 
 require_once(dirname(dirname(__FILE__)) . "/inc/i18n.php");
@@ -15,28 +15,27 @@ use function \EPFL\STI\Theme\___;
 
 class GoogleMap extends \WP_Widget
 {
-    public function __construct()
-    {
-		parent::__construct(
-			'EPFL_STI_Theme_Widget_GoogleMap',   // unique id
-			'EPFL STI Google Map',               // widget title
-			array(
-				'description' => ___( 'Show a Google Map with pins for all the EPFL STI sites' )
-			)
-        );
-	}
+  public function __construct()
+  {
+    parent::__construct(
+      'EPFL_STI_Theme_Widget_GoogleMap',   // unique id
+      'EPFL STI Google Map',               // widget title
+      array(
+        'description' => ___( 'Show a Google Map with pins for all the EPFL STI sites' )
+      )
+    );
+  }
 
-    public function widget ($args, $config)
-    {
-        echo $args['before_widget'];
+  public function widget ($args, $config)
+  {
+    echo $args['before_widget'];
 ?>
-  <script src="<?= get_template_directory_uri() . '/js/google-maps.js' ?>"></script>
-  <div id="googlemap"></div>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-KB1byyUR6AEWVI1B8cdGFIDI1v8g8YY&libraries=places&callback=initMap" async defer></script>
-
+    <script src="<?= get_template_directory_uri() . '/js/google-maps.js' ?>"></script>
+    <div id="googlemap"></div>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-KB1byyUR6AEWVI1B8cdGFIDI1v8g8YY&libraries=places&callback=initMap" async defer></script>
 <?php
-        echo $args['after_widget'];
-    }  // public function Widget
-}      // class GoogleMap
+    echo $args['after_widget'];
+  }  // public function Widget
+}  // class GoogleMap
 
 register_widget(GoogleMap::class);
