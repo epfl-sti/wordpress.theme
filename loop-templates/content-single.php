9c1d197465a724e9b29e0ea8653720ca416b5b47
John Maxwell <john.maxwell@epfl.ch>
2018-01-23 08:45:13 +0100

    basic content

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -10,22 +10,17 @@
 
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="entry-meta">
-
-			<?php epflsti_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-	<div class="entry-content">
-FOLLOWS
+	<div class="entry-content sti_content_maincolumn">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<ul>
+		<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 		<?php the_content(); ?>
-
+</ul>
 		<?php
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
@@ -35,6 +30,11 @@ FOLLOWS
 
 	</div><!-- .entry-content -->
 
+		<div class="entry-meta">
+
+			<?php epflsti_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
 	<footer class="entry-footer">
 
 		<?php epflsti_entry_footer(); ?>
