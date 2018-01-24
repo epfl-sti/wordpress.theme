12213d933f713cf3d9343f42becbd50ae4647b7b
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-24 20:59:31 +0100

    PHPify this
    * Set most options to "inherit." One is still able to copy and modify
      them in a custom theme
    * Use a namespace
    * Remove nonce numbers

--- inc/maxmegamenu.php
+++ inc/maxmegamenu.php
@@ -5,56 +5,63 @@
  * @package epflsti
  */
 
+namespace EPFL\STI;
+
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
-function megamenu_add_theme_epfl_sti_mega_menu_1516816083($themes) {
-  $themes["epfl_sti_mega_menu_1516816083"] = array(
+function megamenu_setup_theme ($themes) {
+    error_log("Setting up megamenu theme");
+  $themes["epfl_sti_mega_menu"] = array(
     'title' => 'EPFL STI Mega Menu',
-    'container_background_from' => 'rgb(255, 255, 255)',
-    'container_background_to' => 'rgb(255, 255, 255)',
     'menu_item_align' => 'center',
-    'menu_item_background_hover_from' => 'rgb(136, 136, 136)',
-    'menu_item_background_hover_to' => 'rgb(136, 136, 136)',
-    'menu_item_link_font' => 'Comic Sans MS, cursive, sans-serif',
-    'menu_item_link_color' => 'rgb(74, 201, 70)',
     'menu_item_highlight_current' => 'off',
-    'panel_header_border_color' => '#555',
-    'panel_font_size' => '14px',
-    'panel_font_color' => '#666',
-    'panel_font_family' => 'inherit',
-    'panel_second_level_font_color' => '#555',
-    'panel_second_level_font_color_hover' => '#555',
-    'panel_second_level_text_transform' => 'uppercase',
-    'panel_second_level_font' => 'inherit',
-    'panel_second_level_font_size' => '16px',
-    'panel_second_level_font_weight' => 'bold',
-    'panel_second_level_font_weight_hover' => 'bold',
-    'panel_second_level_text_decoration' => 'none',
-    'panel_second_level_text_decoration_hover' => 'none',
-    'panel_second_level_border_color' => '#555',
-    'panel_third_level_font_color' => '#666',
-    'panel_third_level_font_color_hover' => '#666',
-    'panel_third_level_font' => 'inherit',
-    'panel_third_level_font_size' => '14px',
-    'flyout_link_size' => '14px',
-    'flyout_link_color' => '#666',
-    'flyout_link_color_hover' => '#666',
-    'flyout_link_family' => 'inherit',
-    'line_height' => 'inherit',
     'z_index' => '10',
-    'toggle_background_from' => '#222',
-    'toggle_background_to' => '#222',
-    'toggle_font_color' => 'rgb(74, 201, 70)',
+    'panel_second_level_text_transform' => 'uppercase',
+    'mobile_menu_item_link_text_align' => 'left',
+
+    /* Cut back on the plug-in's micromanagement of dimensions, fonts
+     * and colors: */
+    'panel_second_level_font'                  => 'inherit',
+    'panel_second_level_font_size'             => 'inherit',
+    'panel_second_level_font_weight'           => 'inherit',
+    'panel_second_level_font_weight_hover'     => 'inherit',
+    'panel_second_level_text_decoration'       => 'inherit',
+    'panel_second_level_text_decoration_hover' => 'inherit',
+    'toggle_background_from'                   => 'inherit',
+    'toggle_background_to'                     => 'inherit',
+    'mobile_menu_item_link_font_size'          => 'inherit',
+    'mobile_menu_item_link_color'              => 'inherit',
+    'panel_third_level_font'                   => 'inherit',
+    'flyout_link_family'                       => 'inherit',
+    'flyout_link_size'                         => 'inherit',
+    'line_height'                              => 'inherit',
+    'panel_third_level_font_size'              => 'inherit',
+
+    /* We can safely apply the same treatment to colors, except if
+     * they are part of a gradient: */
+    'toggle_font_color'                        => 'inherit',
+    'panel_second_level_border_color'          => 'inherit',
+    'panel_third_level_font_color'             => 'inherit',
+    'panel_third_level_font_color_hover'       => 'inherit',
+    'flyout_link_color'                        => 'inherit',
+    'flyout_link_color_hover'                  => 'inherit',
+
+    /* Colors that we want to set: */
+    'container_background_from' => 'white',
+    'container_background_to' => 'white',
     'mobile_background_from' => '#222',
     'mobile_background_to' => '#222',
-    'mobile_menu_item_link_font_size' => '14px',
-    'mobile_menu_item_link_color' => '#ffffff',
-    'mobile_menu_item_link_text_align' => 'left',
-    'custom_css' => '/** Push menu onto new line **/
+    'menu_item_background_hover_from' => 'rgb(238, 238, 238)',
+    'menu_item_background_hover_to' => 'rgb(238, 238, 238)',
+    'menu_item_link_color' => 'rgb(74, 201, 70)',
+
+
+    'custom_css' => '
       megamenu #{$wrap} {
+        /** Push menu onto new line **/
         clear: both;
         #{$menu} > li.mega-menu-item {
           > a.mega-menu-link {
@@ -74,4 +81,4 @@ function megamenu_add_theme_epfl_sti_mega_menu_1516816083($themes) {
   );
   return $themes;
 }
-add_filter("megamenu_themes", "megamenu_add_theme_epfl_sti_mega_menu_1516816083");
+add_filter("megamenu_themes", "\\EPFL\\STI\\megamenu_setup_theme");
