fcad4981ca0bc0b052961e99430fe7a0adac40d5
John Maxwell <john.maxwell@epfl.ch>
2018-01-25 09:46:47 +0100

    standard content page without box of links on the right

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -14,25 +14,56 @@
 
 	</header><!-- .entry-header -->
 
+         <div class=container>
+           <div class=row>
 
-	<div class="entry-content sti_content_maincolumn">
+	    <div class="entry-content standard-content-box">
+     <img class="ribbon-red-top" src="/wp-content/themes/epfl-sti/img/src/topright.png">
+     <img class="ribbon-red-bottom" src="/wp-content/themes/epfl-sti/img/src/bottomleft.png">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-<ul>
-		<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-		<?php the_content(); ?>
-</ul>
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
-			'after'  => '</div>',
-		) );
-		?>
+ 		    <div class="container">
+			<div class="row entry-body">
+			      <div class="col-xs-6 standard-margin">
+				<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+			      </div><!-- col  -->
+			      <div class="col-xs-6 standard-margin">
+				<?php the_content(); ?>
+			      </div><!-- col  -->
+			</div><!-- row -->
+				<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
+					'after'  => '</div>',
+				) );
+				?>
+		     </div> <!-- entry-body-->
+  	    </div><!-- .entry-content -->
 
-	</div><!-- .entry-content -->
+	    <div class="entry-content standard-content-box">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+ 		    <div class="container">
+			<div class="row entry-body">
+			      <div class=col-xs-6>
+				<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+			      </div><!-- col  -->
+			      <div class=col-xs-6>
+				<?php the_content(); ?>
+			      </div><!-- col  -->
+			</div><!-- row -->
+				<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'epflsti' ),
+					'after'  => '</div>',
+				) );
+				?>
+		     </div> <!-- entry-body-->
+  	    </div><!-- .entry-content -->
+	 </div><!-- row-->
+        </div><!-- container-->
 
 		<div class="entry-meta">
 
-			<?php epflsti_posted_on(); ?>
+			<!---?php epflsti_posted_on(); --->
 
 		</div><!-- .entry-meta -->
 	<footer class="entry-footer">
