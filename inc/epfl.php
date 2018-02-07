d5b76cbff6a9edbe9b075e9b78b1a71b96473fb2
Nicolas Borboën <nicolas.borboen@epfl.ch>
2018-02-07 15:44:34 +0100

    [fix] empty matches when using wp cli
    * `Notice: Undefined offset: 0 in
    /srv/test/localhost/htdocs/wp-content/themes/epfl-sti/inc/epfl.php on
    line 134`

--- inc/epfl.php
+++ inc/epfl.php
@@ -131,7 +131,7 @@ function get_institute() {
   $path = parse_url($url, PHP_URL_PATH);
   $re = '/\/institute?s\/([^\/]*)/';
   preg_match_all($re, $path, $matches, PREG_SET_ORDER, 0);
-  return $matches[0][1];
+  return isset($matches[0][1]) ? $matches[0][1] : '' ;
 }
 
 const INSTITUTES = array(
