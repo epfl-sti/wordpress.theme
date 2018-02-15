732a03c39daa282b13e3cef424453717ecaa2f37
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-02-15 18:05:07 +0100

    Contact widget: omit fields with no value

--- widgets/contact.php
+++ widgets/contact.php
@@ -95,24 +95,26 @@ class Contact extends \WP_Widget
   // Display the widget
   public function widget( $args, $instance )
   {
-    extract( $args );
-    foreach(self::FIELDS as $field) {
-      $$field = isset( $instance[$field] ) ? apply_filters( 'widget_text', $instance[$field] ) : '';
-    }
+    $title = isset( $instance["title"] ) ? apply_filters( 'widget_text', $instance["title"] ) : '';
+    $name  = isset( $instance["name"] )  ? apply_filters( 'widget_text', $instance["name"] )  : '';
     echo $args['before_widget'];
     ?>
     <div class="widget epfl-sti-contact">
       <div itemscope itemtype="https://schema.org/ContactPoint">
-        <?php echo $before_title . $title . $after_title; ?>
+        <?php echo $args["before_title"] . $title . $args["after_title"]; ?>
         <address>
           <div itemscope itemtype="schema.org/PostalAddress">
+          <?php if ($name): ?>
             <strong><span property="name"><?php echo $name; ?></span></strong><br />
-            <span itemprop="contactType"><?php echo $contactType; ?></span><br />
-            <span itemprop="email"><?php echo $email; ?></span><br />
-            <span itemprop="telephone"><?php echo $telephone; ?></span><br />
-            <span itemprop="streetAddress"><?php echo $streetAddress; ?></span><br />
-            <span itemprop="postOfficeBoxNumber"><?php echo $postOfficeBoxNumber; ?></span><br />
-            <span itemprop="addressCountry"><?php echo $addressCountry; ?></span>-<span itemprop="postalCode"><?php echo $postalCode; ?></span> <span itemprop="addressLocality"><?php echo $addressLocality; ?></span><br />
+          <?php endif;
+            foreach(self::FIELDS as $field) {
+                if ($field === "name" or $field === "title") { continue; }
+                $fieldval = isset( $instance[$field] ) ? apply_filters( 'widget_text', $instance[$field] ) : '';
+                if ($fieldval) {
+                    printf("<span itemprop=\"%s\">%s</span><br />\n", $field, $fieldval);
+                }
+            }
+          ?>
           </div>
         </address>
       </div>
