8e1c2b0383e74c28c2c3b6c2a236272d31f45482
Nicolas Borboën <nicolas.borboen@epfl.ch>
2018-02-16 00:49:06 +0100

    [add] more exclusions for target=_blank

--- js/target_blank.js
+++ js/target_blank.js
@@ -1,1 +1,8 @@
-$( 'a:not([href^="/"], [href^="https://sti-test."], [href^="https://localhost"], [href^="\#"])').attr({target: "_blank"});
+$('a:not(' +
+  '[href^="/"], ' +
+  '[href^="https://sti.epfl"], ' +
+  '[href^="https://sti-test.epfl"], ' +
+  '[href^="https://sti-dev.epfl"], ' +
+  '[href^="https://localhost"], ' +
+  '[href^="\#"], ' +
+')').attr({target: "_blank"});
