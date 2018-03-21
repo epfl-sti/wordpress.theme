4f19893b2325a044cf996b648382e2a80cb17864
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-03-21 18:22:55 +0100

    Extract function curl_get_json()

--- inc/epfl.php
+++ inc/epfl.php
@@ -108,23 +108,23 @@ function curl_get_utf8 ($url)
     }
 }
 
+function curl_get_json ($url) {
+  return json_decode(curl_get($url));
+}
+
 /* Retrieve events from the Memento REST api */
 // Note: idk what's the "published" entry, but it would be nicer to be sure
 //       to only fetch those one.
 function get_events_from_memento($url='https://memento.epfl.ch/api/jahia/mementos/sti/events/en/?format=json', $limit=4)
 {
-  $data = curl_get($url . '&limit=' . $limit);
-  $data = json_decode($data);
-  return $data;
+    return curl_get_json($url . '&limit=' . $limit);
 }
 
 // To adapt to your school, change from https://actu.epfl.ch/feeds/rss/STI/en/ to e.g. http://actu.epfl.ch/api/jahia/channels/enac/news/fr/?format=json
 // 'https://actu.epfl.ch/api/v1/channels/10/news/?format=json&lang='.$lang.'&category=3&faculty=3&themes=4';
 function get_news_from_actu($url='https://actu.epfl.ch/api/jahia/channels/sti/news/en/?format=json', $limit=3)
 {
-  $data = curl_get($url . '&limit=' . $limit);
-  $data = json_decode($data);
-  return $data->results;
+    return curl_get_json($url . '&limit=' . $limit)->results;
 }
 
 // To return the current institute's acronym. Used to load the relevant menu.
