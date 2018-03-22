bae071f38073d6c5a3f8302e3f699db285eca6b9
Dominique Quatravaux <dominique@quatravaux.org>
2018-03-22 17:20:23 +0100

    Use new epflsti_lab_card template function to render [Labs]

--- inc/epfl-ws-shortcodes.php
+++ inc/epfl-ws-shortcodes.php
@@ -10,6 +10,9 @@ if ( ! defined( 'ABSPATH' ) ) {
     die( 'Access denied.' );
 }
 
+require_once(__DIR__ . "/template-tags.php");
+use function \epflsti_lab_card;
+
 add_filter(
     "epfl_shortcode_memento_list_html_item",
     function ($unused_html, $unused_shortcode_attrs, $item) {
@@ -95,11 +98,5 @@ add_filter("epfl_shortcode_actu_list_html_item", function ($unused_html, $unused
 }, 10, 3);
 
 add_filter("epfl_shortcode_labs_list_html_item", function ($unused_html, $unused_shortcode_attrs, $lab) {
-    $name        = $lab->get_name();
-    $abbrev      = $lab->get_abbrev();
-    $website_url = $lab->get_website_url();
-
-    return "<div class=\"fullwidth-list-item\">
-        <h2><a href=\"$website_url\">$name (<span class=\"lab-abbrev\">$abbrev</span>)</a></h2>
-       </div>";
+    return epflsti_lab_card(null, array("lab" => $lab));
 }, 10, 3);
