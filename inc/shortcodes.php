1d6ce4464ab8d9bceef2a59b45588ad9d7b1c926
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-03-22 18:28:38 +0100

    Remove stray error_log's

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -17,9 +17,7 @@ use EPFL\WS\Labs\Lab;
 
 add_shortcode("lab-card", function ($attrs, $content) {
     if ($attrs["abbrev"] && class_exists('\\EPFL\\WS\\Labs\\Lab')) {
-        error_log("Looking up by abbrev on " . $attrs["abbrev"]);
         $attrs["lab"] = Lab::get_by_abbrev($attrs["abbrev"]);
-        error_log("Result: " . var_export($attrs["lab"], true));  // XXX
     }
     $html = epflsti_lab_card($content, $attrs);
     return $html;
