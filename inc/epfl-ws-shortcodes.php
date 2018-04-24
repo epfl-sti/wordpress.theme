7608fd7f9b4ff6c20e516b7269f4621835e2767b
Nicolas Borboën <ponsfrilus@gmail.com>
2018-04-24 15:38:42 +0200

    Actu layout based on the same as memento
     * get rid of the "get_anchor_link" awful function by usung
    $item->news_url
     * Correct spacing and layout with responsive image thanks to B4 classes

--- inc/epfl-ws-shortcodes.php
+++ inc/epfl-ws-shortcodes.php
@@ -75,31 +75,25 @@ add_filter(
 
     }, 10, 3);
 
-function title2anchor ($title)
-{
-    $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-                                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-                                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
-                                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
-    $anchor = strtr( $title, $unwanted_array );
-    $anchor = str_replace(" ", "-", $anchor);
-    $anchor = str_replace("'", "-", $anchor);
-    $anchor = strtolower($anchor);
-    $anchor = substr($anchor, 0, 50);
-    return $anchor;
-}
-
 add_filter("epfl_shortcode_actu_list_html_item", function ($unused_html, $unused_shortcode_attrs, $item) {
-        $link_to_article = "<a href=\"https://actu.epfl.ch/news/" . title2anchor($item->title) . "\">";
-        $publication_date = date("jS F, Y", strtotime($item->publish_date));
-        return "<div class=\"fullwidth-list-item\">
-         <h2>$link_to_article".strtoupper($item->title)."</a><span class=\"top-right\">" . $publication_date . "</span></h2>
-         <div class=\"actu-details\">
-          <img src=\"".$item->visual_url."\" width=\"170\" height=\"100\">
-          <span> ".$item->subtitle."</span>
-         </div>
-        </div>";
+
+    $publication_date = date("jS F, Y", strtotime($item->publish_date));
+    return "<div class=\"fullwidth-list-item\" id=\"" . $item->id . "\">
+      <h2><a href=\"" . $item->news_url . "\">" . strtoupper($item->title) . "</a><span class=\"top-right\">$publication_date</span></h2>
+      <div class=\"container\">
+        <div class=\"row entry-body\">
+          <div class=\"col-md-2\">
+            <div class=\"text-center actu-details\">
+              <a href=\"" . $item->news_url . "\"><img src=\"" . $item->visual_url . "\" title=\"" . $item->image_description . "\" /></a>
+            </div>
+          </div>
+          <div class=\"col-md-10 actu-item-content\">
+            " . $item->subtitle . "
+            <span class=\"float-right\"><a href=\"" . $item->news_url . "\">Read more</a></span>
+          </div>
+        </div>
+      </div>
+    </div>";
 }, 10, 3);
 
 add_filter("epfl_shortcode_labs_list_html_item", function ($unused_html, $unused_shortcode_attrs, $lab) {
