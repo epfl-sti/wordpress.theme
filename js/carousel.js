004b598239e5bfc9231b94c206fab31cbea32f0d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-05-03 14:28:03 +0200

    Carousel legends on top of the red wave (almost)
    * Both position: <something> *and* a z-index are required, both for
      ancestors (a.k.a. "stacking contexts") and descendants that want to
      appear in a different z-order than the default one
    
    * Assuming the above is taken care of, higher z-index means closer to
      the observer's eye
    
    * Kludge to speed up the carousel transition's end phase. By default,
      Bootstrap leaves the transitioning CSS classes on for another 600
      ms (not sure why - Perhaps to accomodate older browsers?), and
      since these classes use the CSS 'transition': attribute this breaks
      the z-index business ("creates a new stacking context", in
      Standardese) and the legend slips under the wave

--- js/carousel.js
+++ js/carousel.js
@@ -13,4 +13,16 @@ jQuery(function ($) {
         },
         allowPageScroll:"vertical"
     });
+
+   $('.carousel').on('slid.bs.carousel', function() {
+       // Finish the animation ASAP — An animating div.carousel-item
+       // has a "transition" CSS property, which blocks z-index bliss
+       // ("creates a stacking context", in standardese) and therefore
+       // causes the legend to render below the red wave, which is
+       // unfortunate.
+       //
+       // (For some reason, Bootstrap carousel would wait for an
+       // additional TRANSITION_DURATION = 600 ms here)
+       $('.carousel-item').removeClass('carousel-item-right carousel-item-left carousel-item-next carousel-item-prev')
+   });
 });
