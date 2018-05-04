0cc4884ff8301487130ac56dbd3ee9ce66c44c50
John Maxwell <john.maxwell@epfl.ch>
2018-05-04 11:36:41 +0200

    News page correct title format

--- loop-templates/content-single-post.php
+++ loop-templates/content-single-post.php
@@ -12,23 +12,35 @@
 
 ?>
 <div class=container><?php # row if there is a box of links on the right ?>
-  <div class="row entry-body" style="background-color:white"><?php #  container if there is a box of links on the right ?>
-    <div class="col-md-8">
-	  <card>
+  <div class="row entry-body"><?php #  container if there is a box of links on the right ?>
+    <div class="col-md-12">
+
+
+
+
+ 	  <header class="sti-textured">
 	    <?php the_title( '<h1>', '</h1>' ); ?>
-	    <main>
+ 	  </header>
+
+    <div class=container>
+      <div class="row entry-body" style="background-color:white"><?php #  container if there is a box of links on the right ?>
+        <div class="col-md-8">
+
+
+        <main>
 	    <?php the_content(); ?>
 	    </main>
-          </card>
-</div>
-<div class="col-md-4">
-<div class="bdr"> 	      <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?></div>
-</div>
 
+      </div>
+      <div class="col-md-4">
+         <div class="bdr"> 	      <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?></div>
+      </div>
 
-    </div><?php #  main row ?>
-      </div><?php #  main container ?>
-
+     </div><?php #  row ?>
+    </div><?php #  container ?>
+  </div><?php #  main col ?>
+ </div><?php #  main row ?>
+</div><?php #  main container ?>
 
 	<footer>
 
