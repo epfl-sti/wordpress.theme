a1178c87cef8c1605969038513eb51c46e5d6556
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-05-07 08:47:19 +0200

    Z-index-clean carousel transitions with margin-left
    * Countermand all "transform: translateX" - based transitions from
      Bootstrap's _variables.scss and _carousel.scss; set up
      "margin-left:" based surrogates instead
    
    * As per the MDN, the browser is free to render animations directly in
      the final state (bypassing the animation) when an element goes from
      display: none to display: block, which the .carousel-item-next
      does (although it beats me why it doesn't do the exact same thing
      for the original, translateX-based animation). The way to overcome
      that is to set display: block first, style to the beginning of the
      animation, and "a handful of milliseconds" later, set a class that
      styles to the end of the animation (class "ready-set-go" in
      carousel.js).
    
    * Additional comments to teach myself what I keep forgetting between
      sessions

--- js/carousel.js
+++ js/carousel.js
@@ -14,4 +14,21 @@ jQuery(function ($) {
         allowPageScroll:"vertical"
     });
 
+    // The next carousel item goes from display:none to display:block,
+    // which causes the browser to skip the animation and render
+    // directly in the final state (although that appears to depend on
+    // the specifics of the animation, i.e. a "transform" is treated
+    // differently - Go figure). MDN says to fix that by adding
+    // another class a jiffy later.
+    $('.carousel').on('slide.bs.carousel', function() {
+        var self = this;
+        setTimeout(function() {
+            var $comingNext = $(".carousel-item-next, .carousel-item-prev", self);
+            $comingNext.addClass("ready-set-go");
+            $('.carousel').one('slid.bs.carousel', function() {
+                $comingNext.removeClass("ready-set-go");
+            });
+        }, 1);  // "A handful of milliseconds" as per
+                // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions#JavaScript_examples
+    });
 });
