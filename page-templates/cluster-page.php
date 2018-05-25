d542814912a7b3e9b0728359a482c6c1f96e7daa
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-05-25 09:40:06 +0200

    Basic, Polylang-safe filtering by tag slug
    * Plumb down and properly factor out the WP_Query and WP_Term logic
      for each case (people, news, events, courses etc.)
    
    * Passing the slug of a language-neutral Just Works™
    
    * Passing the slug of a translated tag picks up the one for the
      current language

--- page-templates/cluster-page.php
+++ page-templates/cluster-page.php
@@ -13,42 +13,151 @@
 
 namespace EPFL\STI\ClusterPage;
 
-get_header();
+const CLUSTER_TAG_SLUG_META = "cluster_tag_slug";
+
+function debug ($message)
+{
+    // error_log($message);
+}
+
+function _get_term_by_slug ($slug)
+{
+    if (! $slug) return null;
+    // We can't just
+    //
+    //   return get_term_by('slug', $slug, 'post_tag');
+    //
+    // because Polylang insists (through request filtering) that tags
+    // fetched in this way have the current language. The thing is,
+    // when one creates-as-one-types a tag from a Post or Person etc
+    // wp-admin edit page, Polylang will not attach a language to it.
+    // We want to make such language-neutral tags Just Work™, not
+    // force the user to come around and Polylangify them.
+    $term_array = get_terms(array(
+        'get'             => 'all',
+        'taxonomy'        => 'post_tag',
+        'slug'            => $slug,
+        'suppress_filter' => true,
+        // Rein in Polylang request filtering
+        'lang'            => false
+    ));
+
+    if (count($term_array) > 1) {
+        error_log('Found ' . count($term_array) . ' terms for slug ' . $slug);
+    }
+
+    if (! ($term = $term_array[0])) return null;
+
+    // Still, for those tags that have been Polylangified indeed, we
+    // want automagic translation to occur:
+    if (! (function_exists('pll_get_term_translations') &&
+           function_exists('pll_get_current_language')  &&
+           $translations = pll_get_term_translations($term->term_id))) {
+        return $term;
+    }
+    if ($translated = $translations[pll_get_current_language()]) {
+        return $translated;
+    } else {
+        return $term;
+    }
+}
+
+function _get_cluster_tag ()
+{
+    static $cluster_tag;
+    if (! $cluster_tag) {
+        global $post;
+        $cluster_tag_slug = get_post_meta($post->ID, CLUSTER_TAG_SLUG_META, true);
+        debug("Cluster tag slug is $cluster_tag_slug");
+        $cluster_tag = _get_term_by_slug($cluster_tag_slug);
+        debug('$cluster_tag is ' . var_export($cluster_tag, true));
+    }
+    return $cluster_tag;
+}
+
+function _wpquery ($additional_wp_query_params)
+{
+    if (! ($cluster_tag = _get_cluster_tag())) {
+        // https://wordpress.stackexchange.com/a/140727/132235
+        return new \WP_Query(array('post__in' => array(0)));
+    }
+    $query_params = array_merge(
+        array(
+            "tag_id"         => $cluster_tag->term_id,
+            "posts_per_page" => -1
+        ),
+        $additional_wp_query_params);
+
+    debug("Query params: " . var_export($query_params, true));
+
+    return new \WP_Query($query_params);
+}
+
+function _have_cluster_items ($additional_wp_query_params)
+{
+    $q = _wpquery($additional_wp_query_params);
+
+    global $epflsti_cluster_items;
+    $epflsti_cluster_items = $q->get_posts();
+    debug("\$epflsti_cluster_items has " . count($epflsti_cluster_items) . " items");
+    return (count($epflsti_cluster_items) > 0);
+}
 
 function have_cluster_people ()
 {
-    return true;
+    return _have_cluster_items(array(
+        "post_type" => "epfl-person"
+    ));
 }
 
 function have_cluster_labs ()
 {
-    return false;
+    return _have_cluster_items(array(
+        "post_type" => "epfl-lab"
+    ));
 }
 
 function have_cluster_news ()
 {
-    return false;
+    return _have_cluster_items(array(
+        "post_type" => array("epfl-actu", "post")
+    ));
 }
 
 function have_cluster_events ()
 {
-    return false;
+    return _have_cluster_items(array(
+        "post_type" => "epfl-memento"
+    ));
 }
 
 function have_cluster_courses ()
 {
-    return false;
+    return _have_cluster_items(array(
+        "post_type" => "epfl-course"
+    ));
 }
 
 function have_cluster_media ()
 {
-    return false;
+    return _have_cluster_items(array(
+        "post_type" => "post",
+        "tax_query" => array(
+            'taxonomy' => 'post_format',
+            'field'    => 'slug',
+            'terms'    => array( 'post-format-video' )
+        )
+    ));
 }
 
 function render_cluster_items ()
 {
   ?><img src="https://www.codeschool.com/assets/custom/geocities/underconstruction-72327f17c652569bab9a33536622841bf905d145ee673a3e9d065fae9cabfe4f.gif"><?php
 }
+
+###############################################################################
+
+get_header();
 
 ?>
 
