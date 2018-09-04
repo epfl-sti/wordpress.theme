2c2e43a124b7349fe92a734bb422d7c4fd559078
Nicolas Borboën <ponsfrilus@gmail.com>
2018-09-04 14:49:05 +0200

    Rewriting of the search result template
    * Fix missing div
    * Use of the post date as for actu/memento
    * Different content for post and page
    * Use of post/page featured image
    * If logged, display "Edit" link

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -7,35 +7,35 @@
 
 ?>
 <!-- epflsti:loop-templates/content-search.php -->
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-	<header>
-
-		<?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-
-			<div class="search-result-meta">
-
-				<?php epflsti_posted_on(); ?>
-
-			</div>
-
-		<?php endif; ?>
-
-	</header>
-
-	<div class="search-result-summary">
-
-		<?php the_excerpt(); ?>
-
-	</div>
-
-	<footer>
-
-		<?php epflsti_entry_footer(); ?>
-
-	</footer>
-
-</article><!-- #post-## -->
+<div class="fullwidth-list-item" id="post-<?php the_ID(); ?>">
+  <h2>
+    <a href="<?php esc_url( get_permalink() )  ?>" rel="bookmark"><?php the_title() ?></a>
+    <span class="top-right"><?php echo get_the_date( 'jS F, Y' ); ?></span>
+  </h2>
+  <div class="container">
+    <div class="row entry-body">
+      <div class="col-md-2">
+        <div class="text-center actu-details search-details">
+          <a href="<?php echo esc_url( get_permalink() ); ?>"><?php echo get_the_post_thumbnail(get_the_ID(), 'entry_without_sidebar'); ?></a>
+        </div>
+      </div>
+      <div class="col-md-10 actu-item-content">
+        <div class="search-result-summary">
+          <?php if ( 'post' == get_post_type() ) : ?>
+            <div class="search-result-meta">
+              <?php the_excerpt(); ?>
+            </div>
+          <?php else: ?>
+            <div class="search-result-meta">
+              Page: <a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
+            </div><br />
+          <?php endif; ?>
+        </div>
+        <div class="actu-authors text-right font-italic">
+          <?php epflsti_entry_footer(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of epflsti:loop-templates/content-search.php -->
--- search.php
+++ search.php
@@ -11,56 +11,64 @@ $container   = get_theme_mod( 'epflsti_container_type' );
 $sidebar_pos = get_theme_mod( 'epflsti_sidebar_position' );
 ?>
 <!-- epflsti:search.php -->
-<div class="wrapper" id="search-wrapper">
+<div class="row">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="col-md-12 content-area" id="primary">
 
-		<div class="row">
+		<main class="site-main" id="main">
 
-			<main class="site-main" id="main">
+			<div class="wrapper" id="search-wrapper">
 
-				<?php if ( have_posts() ) : ?>
+				<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-					<header class="page-header">
-						
-							<h1 class="page-title"><?php printf(
-							/* translators:*/
-							 esc_html__( 'Search Results for: %s', 'epflsti' ),
-								'<span>' . get_search_query() . '</span>' ); ?></h1>
-
-					</header><!-- .page-header -->
-
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php
-						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', 'search' );
-						?>
-
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
+				<?php if ( esc_attr($container) == "container-fluid" ) : ?>
+				<div class="offset-md-1 col-md-10">
 				<?php endif; ?>
 
-			</main><!-- #main -->
+					<div class="row">
 
-			<!-- The pagination component -->
-			<?php epflsti_pagination(); ?>
+						<div class="col-md-12 content-area" id="primary">
 
-		</div><!-- #primary -->
+							<main class="site-main" id="main">
 
-	</div><!-- .row -->
+							<?php if ( have_posts() ) : ?>
+								<header class="page-header">
+									<h1 class="page-title"><?php printf(
+									/* translators:*/
+									 esc_html__( 'Search Results for: %s', 'epflsti' ),
+										'<span>' . get_search_query() . '</span>' ); ?></h1>
+								</header><!-- .page-header -->
+								<?php /* Start the Loop */ ?>
+								<?php while ( have_posts() ) : the_post(); ?>
+									<?php
+									/**
+									 * Run the loop for the search to output the results.
+									 * If you want to overload this in a child theme then include a file
+									 * called content-search.php and that will be used instead.
+									 */
+									get_template_part( 'loop-templates/content', 'search' );
+									?>
+								<?php endwhile; ?>
+							<?php else : // not have_posts() ?>
+								<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+							<?php endif; ?>
 
-</div><!-- Container end -->
+							</main><?php # end main ?>
 
-</div><!-- Wrapper end -->
+						</div><?php # end col ?>
 
+					</div><?php # end row ?>
+
+				<?php if ( esc_attr($container) == "container-fluid" ) : ?>
+				</div>
+				<?php endif; ?>
+
+				<?php epflsti_pagination(); ?>
+
+				</div><?php # end #content ?>
+
+			</div><?php # end wrapper ?>
+		</main><?php # end main ?>
+	</div><?php # end primary ?>
+</div><?php # end row ?>
 <?php get_footer(); ?>
