34dd6b42049a8197135d3c9df9fa6abfada8c675
Nicolas Borboën <ponsfrilus@gmail.com>
2018-09-27 13:01:15 +0200

    [btl] aligning stuff

--- widgets/frontrow.php
+++ widgets/frontrow.php
@@ -40,53 +40,53 @@ class FrontRow extends \WP_Widget
     public function render_header_1 ()
     {
         if ($this->institute) {
-           ?>
-	  <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
-            <?php echo ___('News'); ?><br />
-          </div>
-           <?php
+        ?>
+            <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
+                <?php echo ___('News'); ?><br />
+            </div>
+        <?php
         } else {
             list($firstline, $secondline) = split_on_underscore(___("Some _Highlights"));
-           ?>
-          <div class="text-white frontrowtitle">
-           <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
-          </div>
-           <?php
+            ?>
+            <div class="text-white frontrowtitle">
+                <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
+            </div>
+            <?php
         }
     }
 
     public function render_header_2 ()
     {
         if ($this->institute) {
-          ?>
-          <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
-            <?php echo ___('Faculty'); ?><br />
-          </div>
-          <?php
+        ?>
+            <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
+                <?php echo ___('Faculty'); ?><br />
+            </div>
+        <?php
         } else {
             list($firstline, $secondline) = split_on_underscore(___("About the _School"));
-          ?>
-          <div class="text-white frontrowtitle">
-           <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
-          </div>
-          <?php
+            ?>
+            <div class="text-white frontrowtitle">
+                <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
+            </div>
+            <?php
         }
     }
 
     public function render_header_3 ()
     {
         if ($this->institute) {
-            ?>
+        ?>
             <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
-              <?php echo ___('Info'); ?><br />
+                <?php echo ___('Info'); ?><br />
             </div>
-            <?php
+        <?php
         } else {
             list($firstline, $secondline) = split_on_underscore(___("Institutes &amp; _Centres"));
             ?>
-             <div class="text-white frontrowtitle">
-              <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
-             </div>
+            <div class="text-white frontrowtitle">
+                <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
+            </div>
             <?php
         }
     }
@@ -94,17 +94,17 @@ class FrontRow extends \WP_Widget
     public function render_header_4 ()
     {
         if ($this->institute) {
-            ?>
+        ?>
             <div class="text-white frontrowtitle epfl-sti-institute-frontrow-header">
-              <?php echo ___('EVENTS'); ?><br />
+                <?php echo ___('EVENTS'); ?><br />
             </div>
-            <?php
+        <?php
         } else {
             list($firstline, $secondline) = split_on_underscore(___("Upcoming _Events"));
             ?>
-             <div class="text-white frontrowtitle">
-              <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
-             </div>
+            <div class="text-white frontrowtitle">
+                <span class="firstline"><?php echo $firstline; ?></span><br /><span class="secondline"><?php echo $secondline; ?></span>
+            </div>
             <?php
         }
     }
@@ -179,7 +179,7 @@ class FrontRow extends \WP_Widget
         $the_query = new \WP_Query($args);
         if($the_query->have_posts()):
             while($the_query->have_posts()): $the_query->the_post();
-            // TODO; for now we are getting the image from actu in its thumbnail for, but it would bbe better to get it in a bigger way
+                // TODO; for now we are getting the image from actu in its thumbnail for, but it would bbe better to get it in a bigger way
                 echo "\t<div class=\"frontrownews zoomy\" style=\"background-image:url('" . get_post_meta( $post->ID, 'epfl_external_thumbnail', true ) . "');\">\n";
                 echo "\t\t<a class=\"whitelink\" href=\"" . get_post_meta( $post->ID, 'absolute_slug', true ) . "\">\n";
                 echo "\t\t\t<div class=\"frontrownewstitle\">\n";
@@ -191,84 +191,85 @@ class FrontRow extends \WP_Widget
         endif;
     }
 
-  public function widget($args, $config)
-  {
-    echo $args['before_widget'];
-    $cl = get_current_language();
-    ?>
-    <div class="frontrow container">
-      <div class="row no-gutters">
-        <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
-         <?php $this->render_header_1(); ?>
-          <?php
-            $this->getActuFromPost($config['cat'], $this->get_max_actu_count());
-          ?>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
-          <?php
-              $this->render_header_2();
-              $this->render_menu_2();
-          ?>
-        </div>
-        <div class="w-100 d-none d-md-block d-lg-none"></div>
-        <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
-          <?php
-              $this->render_header_3();
-              $this->render_menu_3();
-          ?>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
-          <?php $this->render_header_4(); ?>
-          <div class="container">
-            <?php
-              $events = $this->get_events_from_memento();
-              $max_len = 52;
-              foreach ($events as $event) { ?>
-                <div class="row frontrow_event">
-                  <div class="col-4 frontrow_event_date">
-                    <!-- the icon as a time element -->
-                    <a href="<?php echo $event->absolute_slug; ?>" title="<?php echo $event->title; ?>">
-                      <time datetime="<?php echo $event->event_start_date; ?>" class="icon">
-                        <em><?php echo date("l", strtotime($event->event_start_date));      // Friday ?></em>
-                        <strong><?php echo date("F", strtotime($event->event_start_date));  // January ?></strong>
-                        <span><?php echo date("d", strtotime($event->event_start_date));    // 19 ?></span>
-                      </time>
-                    </a>
-                  </div>
-                  <div class="col-8 frontrow_event_text">
-                    <?php //var_dump($event);
-                      $s = $event->title;
-                      if (strlen($event->title) > $max_len) {
-                          $offset = ($max_len - 3) - strlen($event->title);
-                          $s = substr($event->title, 0, strrpos($event->title, ' ', $offset)) . '…';
-                      };
-                      echo $s;
+    public function widget($args, $config)
+    {
+        echo $args['before_widget'];
+        $cl = get_current_language();
+        ?>
+        <div class="frontrow container">
+            <div class="row no-gutters">
+                <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
+                    <?php 
+                        $this->render_header_1();
+                        $this->getActuFromPost($config['cat'], $this->get_max_actu_count());
                     ?>
-                    <div class="add_to_calendar"><a href="https://memento.epfl.ch/event/export/<?php echo $event->translation_id; ?>/" title="Add to calendar"><i class="far fa-calendar-plus"></i></a></div>
-                  </div>
                 </div>
-            <?php
-              }
-            ?>
-            <div class="row">
-              <div class="col-12 more_events">
-                <a href="https://memento.epfl.ch/sti/?period=7"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/more.png"></a>
-              </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
+                    <?php
+                        $this->render_header_2();
+                        $this->render_menu_2();
+                    ?>
+                </div>
+                <div class="w-100 d-none d-md-block d-lg-none"></div>
+                <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
+                    <?php
+                        $this->render_header_3();
+                        $this->render_menu_3();
+                    ?>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 frontrowcol">
+                    <?php $this->render_header_4(); ?>
+                    <div class="container">
+                        <?php
+                        $events = $this->get_events_from_memento();
+                        $max_len = 52;
+                        foreach ($events as $event) { ?>
+                            <div class="row frontrow_event">
+                                <div class="col-4 frontrow_event_date">
+                                    <!-- the icon as a time element -->
+                                    <a href="<?php echo $event->absolute_slug; ?>" title="<?php echo $event->title; ?>">
+                                        <time datetime="<?php echo $event->event_start_date; ?>" class="icon">
+                                            <em><?php echo date("l", strtotime($event->event_start_date));      // Friday ?></em>
+                                            <strong><?php echo date("F", strtotime($event->event_start_date));  // January ?></strong>
+                                            <span><?php echo date("d", strtotime($event->event_start_date));    // 19 ?></span>
+                                        </time>
+                                    </a>
+                                </div>
+                                <div class="col-8 frontrow_event_text">
+                                    <?php //var_dump($event);
+                                    $s = $event->title;
+                                    if (strlen($event->title) > $max_len) {
+                                        $offset = ($max_len - 3) - strlen($event->title);
+                                        $s = substr($event->title, 0, strrpos($event->title, ' ', $offset)) . '…';
+                                    };
+                                    echo $s;
+                                    ?>
+                                    <div class="add_to_calendar"><a href="https://memento.epfl.ch/event/export/<?php echo $event->translation_id; ?>/" title="Add to calendar"><i class="far fa-calendar-plus"></i></a></div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                        <div class="row">
+                            <div class="col-12 more_events">
+                                <a href="https://memento.epfl.ch/sti/?period=7"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/src/more.png"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-    <?php
-    echo $args['after_widget'];
-  }  // public function widget
+        <?php
+        echo $args['after_widget'];
+    }  // public function widget
   
     /**
      * Outputs the options form on admin
      *
      * @param array $instance The widget options
      */
-    public function form( $instance ) {
+    public function form( $instance ) 
+    {
         ?>
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'cat' ) ); ?>"><?php esc_attr_e( 'Actu category:', 'text_domain' ); ?></label> 
@@ -291,7 +292,8 @@ class FrontRow extends \WP_Widget
      *
      * @return array
      */
-    public function update( $new_instance, $old_instance ) {
+    public function update( $new_instance, $old_instance ) 
+    {
         // processes widget options to be saved
         $instance = array();
         $instance['cat'] = ( ! empty( $new_instance['cat'] ) ) ? sanitize_text_field( $new_instance['cat'] ) : '';
