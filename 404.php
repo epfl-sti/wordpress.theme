010a45d264ee75b99c0186757b6079f0cbd6791f
Nicolas Borboën <ponsfrilus@gmail.com>
2018-11-05 22:07:52 +0100

    [feat] 404 page that actually invite the visitor to continue his browsing.
    * A message in engligh, thanks to @john
    * A collection of links
    * The search box without any title
    * A creative common flickr image of the EPFL rolex learning center.

--- 404.php
+++ 404.php
@@ -6,11 +6,40 @@
  */
 
 get_header();
-
+?>
+<!--
+!!! WHOOPS SORRY !!!
+……………………………………..________
+………………………………,.-‘”……………….“~.,
+………………………..,.-“……………………………..”-.,
+…………………….,/………………………………………..”:,
+…………………,?………………………………………………\,
+………………./…………………………………………………..,}
+……………../………………………………………………,:`^`..}
+……………/……………………………………………,:”………/
+…………..?…..__…………………………………..:`………../
+…………./__.(…..”~-,_…………………………,:`………./
+………../(_….”~,_……..”~,_………………..,:`…….._/
+……….{.._$;_……”=,_…….”-,_…….,.-~-,},.~”;/….}
+………..((…..*~_…….”=-._……”;,,./`…./”…………../
+…,,,___.\`~,……”~.,………………..`…..}…………../
+…………(….`=-,,…….`……………………(……;_,,-”
+…………/.`~,……`-………………………….\……/\
+………….\`~.*-,……………………………….|,./…..\,__
+,,_……….}.>-._\……………………………..|…………..`=~-,
+…..`=~-,_\_……`\,……………………………\
+……………….`=~-,,.\,………………………….\
+…………………………..`:,,………………………`\…………..__
+……………………………….`=-,……………….,%`>–==“
+…………………………………._\……….._,-%…….`\
+……………………………..,<`.._|_,-&“…………….`
+-->
+<?php
 $container   = get_theme_mod( 'epflsti_container_type' );
 $sidebar_pos = get_theme_mod( 'epflsti_sidebar_position' );
 
 ?>
+<!-- epflsti:404.php -->
 
 <div class="wrapper" id="error-404-wrapper">
 
@@ -20,58 +49,103 @@ $sidebar_pos = get_theme_mod( 'epflsti_sidebar_position' );
 
 			<div class="col-md-12 content-area" id="primary">
 
-				<main class="site-main" id="main">
+				<main class="site-main page-whitebg" id="main">
 
 					<section class="error-404 not-found">
 
-						<header class="page-header">
+						<header class="page-header sti-textured">
 
-							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.',
-							'epflsti' ); ?></h1>
+							<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'epflsti' ); ?></h1>
 
 						</header><!-- .page-header -->
 
-						<div class="page-content">
+						<!-- <div class="page-content"> -->
 
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?',
-							'epflsti' ); ?></p>
+							<div class="article-content page-whitebg">
+								
+								<div class="container">
 
-							<?php get_search_form(); ?>
+									<div class="row">
 
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+										<div class="col-md-8">
 
-							<?php if ( epflsti_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+											<br />
+											<p>
+												<?php 
+													esc_html_e( 'Please accept our apologies — this page is not currently available.', 'epflsti' );
+												?>
+											</p>
+											<p>
+												<?php 
+													esc_html_e( 'If you would like to report this missing content, please write to the following address: ', 'epflsti' ); 
+												?>
+												<a href="mailto:stiitweb@groupes.epfl.ch">STI Web Team</a>.
+											</p>
+											<p>
+												<?php 
+													esc_html_e( 'You may also search for similar content on our website:', 'epflsti' ); 
+												?>
+												<?php get_search_form(); ?>
+											</p>
+											<p>&nbsp;</p>
+											<p>
+												While you are here, feel free to browse our information about the 
+												<a href="/the-school/">EPFL School of Engineering</a>, its 
+												<a href="/research/faculty-members/">faculty members</a>, 
+												the five institutes (<a href="https://bioengineering.epfl.ch/">Bioengineering</a>, 
+												<a href="/research/institutes/iel/">Electrical Engineering</a>, 
+												<a href="/research/institutes/imx/">Materials Science & Engineering</a>, 
+												<a href="/research/institutes/igm/">Mechanical Engineering</a> & 
+												<a href="/research/institutes/imt/">Microengineering</a>), 
+												the <a href="/news">latest news</a> or the <a href="/themes/">themes</a> we cover.
+											</p>
 
-								<div class="widget widget_categories">
+										</div>
 
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'epflsti' ); ?></h2>
+										<div class="col-md-4">
+											&nbsp;
+											<img class="bdr" src="/wp-content/themes/epfl-sti/assets/epfl-rolex-photo-by-maartendanial-flickr.jpg" alt="Photo by Maarten Danial (flickr)" title="Photo by Maarten Danial (flickr)"/><!-- https://www.flickr.com/photos/maartendanial/4413618516/in/photolist-7J1X1Y-7J1gDR-8sAL4h-8zKu7U-dTJZvb-o86heo-eig5dY-8FGxPK-Fnv8eS-ei3nNF-8ySVVJ-WYPgqm-ei9Qwx-8AR1Qp-eifxFf-eia8Li-eiamF2-eig63d-8FGqBt-eifxq5-8zKu7Y-eig68Y-eiadex-eifBbb-eifMrm-eifxYJ-eig6t9-eig3uq-ei9j2J-8LyUYF-eifXBN-ei9ToM-eiajgz-eifzB1-ei8ZKd-eiah14-ei9QMT-eia1ek-eiafSr-ei3jqe-8FGwoX-ei3Gfv-eifWQh-eifHMu-eifBxy-8sAJe7-8FKLgJ-eifW7u-eifydh-eiaf72 -->
+										</div>
 
-									<ul>
-										<?php
-										wp_list_categories( array(
-											'orderby'    => 'count',
-											'order'      => 'DESC',
-											'show_count' => 1,
-											'title_li'   => '',
-											'number'     => 10,
-										) );
-										?>
-									</ul>
+									</div>
 
-								</div><!-- .widget -->
+								</div>
+								<?php // the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-							<?php endif; ?>
+								<?php /*if ( epflsti_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 
-							<?php
+									<div class="widget widget_categories">
 
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'epflsti' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+										<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'epflsti' ); ?></h2>
 
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
+										<ul>
+											<?php
+											wp_list_categories( array(
+												'orderby'    => 'count',
+												'order'      => 'DESC',
+												'show_count' => 1,
+												'title_li'   => '',
+												'number'     => 10,
+											) );
+											?>
+										</ul>
 
-						</div><!-- .page-content -->
+									</div><!-- .widget -->
+
+								<?php endif; */ ?>
+
+								<?php
+
+								/* translators: %1$s: smiley */
+								/*$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'epflsti' ), convert_smilies( ':)' ) ) . '</p>';
+								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+
+								the_widget( 'WP_Widget_Tag_Cloud' );*/
+								?>
+
+							</div><!-- .page-content -->
+
+						<!-- </div> --><!-- .page-content -->
 
 					</section><!-- .error-404 -->
 
--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@
 
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="assistive-text" for="s"><?php esc_html_e( 'Search', 'epflsti' ); ?></label>
+	<label class="assistive-text" for="s"><?php //esc_html_e( 'Search', 'epflsti' ); ?></label>
 	<div class="input-group">
 		<input class="field form-control" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'epflsti' ); ?>">
